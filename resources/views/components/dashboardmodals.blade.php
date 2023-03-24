7d48af7fdf5c924d31c2b748cee2558d8ee38759
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-03-24 16:33:06 +0200

    edit modal browse

--- resources/views/components/dashboardmodals.blade.php
+++ resources/views/components/dashboardmodals.blade.php
@@ -35,8 +35,7 @@
                         <ul class="pt-1 mb-2">
                             <li class="p-1 font-xl">
                                 <input type="hidden" name="hiddenid" id="hiddenid">
-                                <input type="submit" class="submit br-xs cursor-p" value="Confirm"
-                                    name="submit">
+                                <input type="submit" class="submit br-xs cursor-p" value="Confirm" name="submit">
                                 <input onclick="document.getElementById('confirmmodal-category').style.display='none'"
                                     class="submit cursor-p br-xs talign-c" value="Cancel">
                             </li>
@@ -47,65 +46,59 @@
         </div>
     </div>
 </div>
+
 {{-- Image Selection Modal --}}
-<!-- Modals -->
+<!-- Modals Main-->
 <div class="modal" id="imageModal" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="content-category-images br-xs">
-
+    <div class="content-category-images br-sm">
         <div>
-            <span id="closeselection" class="exit text-hover-secondary float-r">&times;</span>
+            <span id="closeselection" class="exit font-xl cursor-p pr-1 float-r">&times;</span>
             <ul class="pt-1 mb-2">
                 <li class="p-1 font-xl">
-                    <h1 class="title talign-c font-xl ls-1 text-white">
-                        {{ __('Please select an image') }}
+                    <h1 class="talign-c font-xl ls-1 text-bg">
+                        {{ __('Please select a main image') }}
                     </h1>
                 </li>
                 <li class="p-1 font-xl">
-                    <div id="images-container-wrapper" style="height: 500px; overflow-y: scroll;">
-                        <div class="modal-body bg-bg-dark-1" id="image-container">
-
+                    <div id="images-container-wrapper">
+                        <div class="modal-body" id="image-container">
                         </div>
-                      </div>
-                      <input type="hidden" id="selected-image" name="hidden">
-
+                    </div>
+                    <input type="hidden" id="selected-image" name="hidden">
                 </li>
                 <li class="p-1 font-xl">
-                    <input type="submit" class="submit cursor-p bg-hover-bg-light-2" value="Confirm" name="submit"
+                    <input type="submit" class="modalbutton cursor-p" value="Confirm" name="submit"
                         id="confirmimageselection">
-                    <input type="button" id="closeselection1" class="submit cursor-p talign-c bg-hover-bg-light-2"
-                        value="Cancel">
+                    <input type="button" id="closeselection1" class="modalbutton cursor-p" value="Cancel">
                 </li>
             </ul>
         </div>
     </div>
 </div>
-
+<!-- Modals Search-->
 <div class="modal" id="imageModal1" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="content-category-images">
+    <div class="content-category-images br-sm">
         <div>
-            <span id="closeselection2" class="exit text-hover-secondary float-r">&times;</span>
+            <span id="closeselection2" class="exit font-xl cursor-p pr-1 float-r">&times;</span>
             <ul class="pt-1 mb-2">
                 <li class="p-1 font-xl">
-                    <h1 class="title talign-c font-xl ls-1 text-white">
-                        {{ __('Please select an image') }}
+                    <h1 class="title talign-c font-xl ls-1 text-bg">
+                        {{ __('Please select a search image') }}
                     </h1>
                 </li>
                 <li class="p-1 font-xl">
-                    <div id="images-container-wrapper" style="height: 500px; overflow-y: scroll;">
-                        <div class="modal-body bg-bg-dark-1" id="image-container1">
-
+                    <div id="images-container-wrapper">
+                        <div class="modal-body" id="image-container1">
                         </div>
-                      </div>
-                      <input type="hidden" id="selected-image1" name="hidden">
+                    </div>
+                    <input type="hidden" id="selected-image1" name="hidden">
                 </li>
                 <li class="p-1 font-xl">
-                    <input type="submit" class="submit cursor-p bg-hover-bg-light-2" value="Confirm" name="submit"
+                    <input type="submit" class="modalbutton cursor-p" value="Confirm" name="submit"
                         id="confirmimageselection1">
-                    <input type="button" id="closeselection3" class="submit cursor-p talign-c bg-hover-bg-light-2"
-                        value="Cancel">
+                    <input type="button" id="closeselection3" class="modalbutton cursor-p" value="Cancel">
                 </li>
             </ul>
         </div>
     </div>
 </div>
-
