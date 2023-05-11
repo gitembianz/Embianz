65af3e5e7aa19412a38563a6cdb4e747ad508e09
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-05-11 09:44:12 +0300

    project clean up

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -58,7 +58,6 @@ return array(
     'ParagonIE\\ConstantTime\\' => array($vendorDir . '/paragonie/constant_time_encoding/src'),
     'NunoMaduro\\Collision\\' => array($vendorDir . '/nunomaduro/collision/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
-    'Livewire\\' => array($vendorDir . '/livewire/livewire/src'),
     'League\\MimeTypeDetection\\' => array($vendorDir . '/league/mime-type-detection/src'),
     'League\\Flysystem\\Local\\' => array($vendorDir . '/league/flysystem-local'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
