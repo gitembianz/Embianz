cb07946457676d7db76f2aed92badd3b6a90fc8e
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-06-06 08:58:03 +0300

    modify

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -10,6 +10,6 @@
 </div>
 </section>
 {{-- page content end --}}
-<x-dashboardright :todolists="$todolists">
+<x-dashboardright :todolists="$todolists" />
 <x-dashboardscript />
 <x-dashboardfooter />
