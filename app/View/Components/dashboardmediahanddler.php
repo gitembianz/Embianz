785500e12fe37545f4144091aca261cb8df94325
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-06-08 08:38:40 +0300

    Modify all components

--- app/View/Components/dashboardmediahanddler.php
+++ app/View/Components/dashboardmediahanddler.php
@@ -7,7 +7,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class dashboardmediahanddler extends Component
+class Dashboardmediahanddler extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/dashboardscript.php
+++ app/View/Components/dashboardscript.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class dashboardscript extends Component
+class Dashboardscript extends Component
 {
     /**
      * Create a new component instance.
