1ade142c921fdc52e41798e1e231e530cbd7b21f
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-06-12 12:24:34 +0300

    Show/hide columns

--- resources/views/livewire/productstable.blade.php
+++ resources/views/livewire/productstable.blade.php
@@ -1,18 +1,18 @@
 <div class="item">
-  @if (session()->has('message'))
-    <div class="alert__session" id="alertevent">
-        <span class="alert__session-text">{!! session('message') !!}</span>
-        <button class="alert__session-btn" type="button"
-            onclick="document.getElementById('alertevent').style.display='none'" data-bs-dismiss="alert"
-            aria-hidden="true">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24" fill="none"
-                stroke="#BBFCDE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-        </button>
-    </div>
-@endif
+    @if (session()->has('message'))
+        <div class="alert__session" id="alertevent">
+            <span class="alert__session-text">{!! session('message') !!}</span>
+            <button class="alert__session-btn" type="button"
+                onclick="document.getElementById('alertevent').style.display='none'" data-bs-dismiss="alert"
+                aria-hidden="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24" fill="none"
+                    stroke="#BBFCDE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+            </button>
+        </div>
+    @endif
     <div class="item__form form-table">
         <div class="item__form-input">
             <input wire:model.debounce.200ms="search" type="text" required>
@@ -28,14 +28,24 @@
             <span>Per Page :</span>
         </div>
 
-        {{-- If you want to delete, its message --}}
+        <div class="dropdown">
+          <span class="dropdown-name">Columns</span>
+
+          <div class="dropdown-content">
+              @foreach ($columns as $column)
+                  <input class="dropdown-item" type="checkbox" wire:model="selectedColumns"
+                      value="{{ $column }}" {{ in_array($column, $selectedColumns) ? 'checked' : '' }}>
+                  <label>{{ $column }}</label>
+              @endforeach
+          </div>
+      </div>
+
         @if ($checked)
             <div class="dropdown">
                 <span class="dropdown-name">With Checked ({{ count($checked) }})</span>
 
                 <div class="dropdown-content">
-                    <button class="dropdown-item delete" type="button"
-                        wire:click="confirmProductsRemovalmultiple()">
+                    <button class="dropdown-item delete" type="button" wire:click="confirmProductsRemovalmultiple()">
                         Delete
                     </button>
                     <button class="dropdown-item submit" type="button"
@@ -68,45 +78,47 @@
     {{-- modals --}}
     {{-- delete single record --}}
     <div class="modal" id="confirmationmodal">
-      <div class="modal-content">
-          <h1 class="modal-content-title">
-              {{ __('Are you sure to delete this product?') }}
-          </h1>
-          <input wire:click.prevent="deleteSingleRecord()" class="modal-content-btn submit" type="button" value="Confirm">
-          <input class="modal-content-btn delete" type="button"
-              onclick="document.getElementById('confirmationmodal').style.display='none'" value="Cancel">
+        <div class="modal-content">
+            <h1 class="modal-content-title">
+                {{ __('Are you sure to delete this product?') }}
+            </h1>
+            <input wire:click.prevent="deleteSingleRecord()" class="modal-content-btn submit" type="button"
+                value="Confirm">
+            <input class="modal-content-btn delete" type="button"
+                onclick="document.getElementById('confirmationmodal').style.display='none'" value="Cancel">
 
-          <span class="modal-content-btn delete"
-              onclick="document.getElementById('confirmationmodal').style.display='none'">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24" fill="none"
-                  stroke="#BBFCDE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
-          </span>
-      </div>
-  </div>
-
-  {{-- delete myltiple records --}}
-  <div class="modal" id="confirmationmodalmultiple">
-    <div class="modal-content">
-        <h1 class="modal-content-title">
-            {{ __('Are you sure to delete those product?') }}
-        </h1>
-        <input wire:click.prevent="deleteRecords()" class="modal-content-btn submit" type="button" value="Confirm">
-        <input class="modal-content-btn delete" type="button"
-            onclick="document.getElementById('confirmationmodalmultiple').style.display='none'" value="Cancel">
-
-        <span class="modal-content-btn delete"
-            onclick="document.getElementById('confirmationmodalmultiple').style.display='none'">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24" fill="none"
-                stroke="#BBFCDE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-        </span>
+            <span class="modal-content-btn delete"
+                onclick="document.getElementById('confirmationmodal').style.display='none'">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24" fill="none"
+                    stroke="#BBFCDE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+            </span>
+        </div>
     </div>
-</div>
+
+    {{-- delete myltiple records --}}
+    <div class="modal" id="confirmationmodalmultiple">
+        <div class="modal-content">
+            <h1 class="modal-content-title">
+                {{ __('Are you sure to delete those product?') }}
+            </h1>
+            <input wire:click.prevent="deleteRecords()" class="modal-content-btn submit" type="button" value="Confirm">
+            <input class="modal-content-btn delete" type="button"
+                onclick="document.getElementById('confirmationmodalmultiple').style.display='none'" value="Cancel">
+
+            <span class="modal-content-btn delete"
+                onclick="document.getElementById('confirmationmodalmultiple').style.display='none'">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
+                    fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+            </span>
+        </div>
+    </div>
 
 
     {{-- end modals --}}
@@ -118,46 +130,67 @@
         <thead>
             <tr>
                 <th><input type="checkbox" wire:model="selectPage"></th>
-                <th class="cursor-p" @if ($orderBy === 'id' && $orderAsc === '1')
-                data-symbol="up"
+                @if ($this->showColumn('Id'))
+                    <th class="cursor-p"
+                        @if ($orderBy === 'id' && $orderAsc === '1') data-symbol="up"
             @else
-                data-symbol="down"
-            @endif wire:click="sortBy('id')">ID
-                </th>
-                <th class="cursor-p" @if ($orderBy === 'name' && $orderAsc === '1')
-                data-symbol="up"
+                data-symbol="down" @endif
+                        wire:click="sortBy('id')">ID
+                    </th>
+                @endif
+                @if ($this->showColumn('Name'))
+                    <th class="cursor-p"
+                        @if ($orderBy === 'name' && $orderAsc === '1') data-symbol="up"
+        @else
+            data-symbol="down" @endif
+                        wire:click="sortBy('name')">Name
+                    </th>
+                @endif
+                @if ($this->showColumn('Short Description'))
+                    <th class="cursor-p"
+                        @if ($orderBy === 'short_description' && $orderAsc === '1') data-symbol="up"
+    @else
+        data-symbol="down" @endif
+                        wire:click="sortBy('short_description')">Short Description
+                    </th>
+                @endif
+                @if ($this->showColumn('Created At'))
+                    <th class="cursor-p"
+                        @if ($orderBy === 'created_at' && $orderAsc === '1') data-symbol="up"
             @else
-                data-symbol="down"
-            @endif wire:click="sortBy('name')">Name
-                </th>
-                <th class="cursor-p" @if ($orderBy === 'short_description' && $orderAsc === '1')
-                data-symbol="up"
-            @else
-                data-symbol="down"
-            @endif wire:click="sortBy('short_description')">Short Description
-                </th>
-                <th class="cursor-p" @if ($orderBy === 'created_at' && $orderAsc === '1')
-                data-symbol="up"
-            @else
-                data-symbol="down"
-            @endif wire:click="sortBy('created_at')">Created At
-                </th>
+                data-symbol="down" @endif
+                        wire:click="sortBy('created_at')">Created At
+                    </th>
+                @endif
+
                 <th></th>
             </tr>
         </thead>
         <tbody>
             @foreach ($products as $product)
                 <tr class="@if ($this->isChecked($product->id)) th_checked @endif">
-                    <td data-title="Check"><input type="checkbox" value="{{ $product->id }}" wire:model="checked"></td>
-                    <td data-title="ID">{{ $product->id }}</td>
-                    <td data-title="Name"><a href="/show_product/{{ $product->id }}'">{{ $product->name }}</a></td>
-                    <td data-title="Short Description">{{ $product->short_description }}</td>
-                    <td data-title="Created At">{{ $product->created_at }}</td>
+                    <td data-title="Check"><input type="checkbox" value="{{ $product->id }}" wire:model="checked">
+                    </td>
+                    @if ($this->showColumn('Id'))
+                        <td data-title="ID">{{ $product->id }}</td>
+                    @endif
+                    @if ($this->showColumn('Name'))
+                        <td data-title="Name"><a href="/show_product/{{ $product->id }}'">{{ $product->name }}</a>
+                        </td>
+                    @endif
+                    @if ($this->showColumn('Short Description'))
+                        <td data-title="Short Description">{{ $product->short_description }}</td>
+                    @endif
+                    @if ($this->showColumn('Created At'))
+                        <td data-title="Created At">{{ $product->created_at }}</td>
+                    @endif
+
+
                     <td data-title="Action">
                         <button class="delete" wire:click.prevent="confirmProductRemoval({{ $product->id }})">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
-                                fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+                                viewBox="0 0 24 24" fill="none" stroke="#BBFCDE" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
                                 <circle cx="12" cy="12" r="10"></circle>
                                 <line x1="15" y1="9" x2="9" y2="15"></line>
                                 <line x1="9" y1="9" x2="15" y2="15"></line>
@@ -168,5 +201,5 @@
             @endforeach
         </tbody>
     </table>
-  <div>{{ $products->links('pagination-links') }} </div>
+    <div>{{ $products->links('pagination-links') }} </div>
 </div>
