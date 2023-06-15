b8ef6fd8846fdd3d051922d32cef8e487b00aba8
Stanislav Cortac <stanislavcortac@gmail.com>
2023-06-15 12:34:49 +0300

    fix z-indexes

--- resources/views/components/dashboardsidebar.blade.php
+++ resources/views/components/dashboardsidebar.blade.php
@@ -4,16 +4,13 @@
         <ul class="sidebar__list">
             <!-- Sidebar Button in the right -->
             <li class="sidebar__open">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24" fill="none"
-                    stroke="#BBFCDE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <svg>
                     <polyline points="9 18 15 12 9 6"></polyline>
                 </svg>
             </li>
             <li><a class="sidebar__item" href="{{ route('dashboard') }}">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <rect x="3" y="3" width="7" height="7"></rect>
                             <rect x="14" y="3" width="7" height="7"></rect>
                             <rect x="14" y="14" width="7" height="7"></rect>
@@ -24,9 +21,7 @@
                 </a></li>
             <li><a class="sidebar__item" href="{{ url('category') }}">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
                             <polyline points="2 17 12 22 22 17"></polyline>
                             <polyline points="2 12 12 17 22 12"></polyline>
@@ -36,9 +31,7 @@
                 </a></li>
             <li><a class="sidebar__item" href="{{ url('products') }}">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <line x1="16.5" y1="9.4" x2="7.5" y2="4.21"></line>
                             <path
                                 d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z">
@@ -51,9 +44,7 @@
                 </a></li>
             <li><a class="sidebar__item" href="#">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <circle cx="9" cy="21" r="1"></circle>
                             <circle cx="20" cy="21" r="1"></circle>
                             <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
@@ -63,9 +54,7 @@
                 </a></li>
             <li><a class="sidebar__item" href="#">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
                             <line x1="3" y1="6" x2="21" y2="6"></line>
                             <path d="M16 10a4 4 0 0 1-8 0"></path>
@@ -75,9 +64,7 @@
                 </a></li>
             <li><a class="sidebar__item" href="#">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z">
                             </path>
                             <line x1="7" y1="7" x2="7.01" y2="7"></line>
@@ -87,9 +74,7 @@
                 </a></li>
             <li><a class="sidebar__item" href="#">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <circle cx="12" cy="12" r="10"></circle>
                             <line x1="2" y1="12" x2="22" y2="12"></line>
                             <path
@@ -101,9 +86,7 @@
                 </a></li>
             <li><a class="sidebar__item" href="#">
                     <button class="sidebar__icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24"
-                            fill="none" stroke="#BBFCDE" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg>
                             <circle cx="12" cy="12" r="3"></circle>
                             <path
                                 d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z">
