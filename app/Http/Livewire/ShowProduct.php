386d75b323c5c612f3ebed41ca2b725c1894076b
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-06-26 22:40:06 +0300

    .view product details with edit posibility

--- app/Http/Livewire/ShowProduct.php
+++ app/Http/Livewire/ShowProduct.php
@@ -11,13 +11,12 @@ class ShowProduct extends Component
 {
 
   public $productId;
-  public $product;
   public $editproduct = null;
+  public $prod =[];
 
   public function mount($productId)
   {
       $this->productId = $productId;
-      $this->product = Product::find($productId);
   }
   public function confirmProductRemoval($id)
   {
@@ -29,9 +28,55 @@ class ShowProduct extends Component
     $this->editproduct = true;
   }
 
+  public function getProductProperty()
+  {
+    return $this->productQuery;
+  }
+
+  public function getProductQueryProperty()
+  {
+    return Product::find($this->productId);
+  }
+
+  public function saveproduct()
+  {
+
+
+    $product_new = $this->prod ?? NULL;
+    if(!is_null($product_new)){
+      $new = Product::find($this->productId);
+      // dd($product_new);
+      if (array_key_exists('product_name', $product_new)) {
+        $new->name = $product_new['product_name'];
+    }if (array_key_exists('product_status', $product_new)) {
+      $new->product_status = $product_new['product_status'];
+  }if (array_key_exists('start_date', $product_new)) {
+    $new->start_date = $product_new['start_date'];
+}if (array_key_exists('end_date', $product_new)) {
+  $new->end_date = $product_new['end_date'];
+}
+if (array_key_exists('quantity', $product_new)) {
+  $new->quantity = $product_new['quantity'];
+}if (array_key_exists('short_description', $product_new)) {
+  $new->short_description = $product_new['short_description'];
+}if (array_key_exists('long_description', $product_new)) {
+  $new->long_description = $product_new['long_description'];
+}
+if (array_key_exists('seo_title', $product_new)) {
+  $new->seo_title = $product_new['seo_title'];
+}
+
+      $new->save();
+      session()->flash('message', 'Product Edited Successfully!');
+    }
+    $this->prod = [];
+    $this->editproduct = null;
+  }
+
   public function cancelproduct()
   {
     $this->editproduct = null;
+    $this->prod = [];
   }
 
   public function deleteSingleRecord()
@@ -57,6 +102,8 @@ class ShowProduct extends Component
 
     public function render()
     {
-        return view('livewire.show-product');
+        return view('livewire.show-product',[
+          'product' => $this->product
+        ]);
     }
 }
--- resources/views/livewire/show-product.blade.php
+++ resources/views/livewire/show-product.blade.php
@@ -1,4 +1,28 @@
 <div>
+   @if (session()->has('message'))
+        <div class="alert__session liveAlert" id="alertevent">
+            <span class="alert__session-text">{!! session('message') !!}</span>
+            <button class="alert__session-btn" type="button" data-bs-dismiss="alert" aria-hidden="true">
+                <svg>
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+            </button>
+        </div>
+        <script>
+            const alertEvent = document.getElementById("alertevent");
+            header.style.marginBottom = '4rem';
+            alertEvent.style.opacity = '1';
+
+            setTimeout(function() {
+                alertEvent.style.opacity = '0';
+                setTimeout(function() {
+                    alertEvent.remove();
+                    header.style.marginBottom = '0';
+                }, 500);
+            }, 2000);
+        </script>
+    @endif
   <div class="item__header">
     <h1 class="item__header-title" id="title">Product- {{ $product->name }}</h1>
     <div class="item__header-buttons">
@@ -42,44 +66,103 @@
   </div>
   <div class="tab__list">
       <div class="tabs__content active" id="Details">
-          <form class="item__form" action="{{ route('category_update', $product->id) }}" method="POST"
-              enctype="multipart/form-data">
-              @csrf
-              <input class="item__form-btn item__form-long" type="submit" style="display: none" value="Update"
-                  id="Update">
-
-              <div class="item__form-input-close">
+              <div class="item__form">
+                @if ($editproduct === null)
+                <div class="item__form-input-close">
                   <div id="category_name">{{ $product->name }}</div>
                   <span>Product Name</span>
               </div>
-              <div class="item__form-input-close">
+                @else
+                <div class="item__form-input">
+                  <input type="text" wire:model.defer="prod.product_name" required>
+                  <span> Product Name</span>
+              </div>
+                @endif
+                @if ($editproduct === null)
+                <div class="item__form-input-close">
                   <div id="category_parrent">{{ $product->product_status }}</div>
                   <span>Product Status</span>
               </div>
-              <div class="item__form-input-close">
+                @else
+                <div class="item__form-input">
+                  <select id="select-category" wire:model.defer="prod.product_status">
+                      <?php
+                      $status = ['active', 'inactive', 'low stock'];
+                      ?>
+                      <option value="" selected="">Select a status</option>
+                      @foreach ($status as $status_name)
+                          <option value="{{ $status_name }}">{{ $status_name }}</option>
+                      @endforeach
+                  </select>
+                  <span>Product Status</span>
+              </div>
+                @endif
+                @if ($editproduct === null)
+                <div class="item__form-input-close">
                   <div id="category_start_date">{{ $product->start_date }}</div>
                   <span for="start_date">Product Start Date</span>
               </div>
-              <div class="item__form-input-close">
+                @else
+                <div class="item__form-input">
+                  <input type="date" id="start_date" wire:model.defer="prod.start_date">
+                  <span>Product Start Date</span>
+              </div>
+                @endif
+                @if ($editproduct === null)
+                <div class="item__form-input-close">
                   <div id="category_end_date">{{ $product->end_date }}</div>
                   <span>Product End Date </span>
               </div>
-              <div class="item__form-input-close">
+                @else
+                <div class="item__form-input">
+                  <input type="date" id="end_date" wire:model.defer="prod.end_date">
+                  <span>Product End Date</span>
+              </div>
+                @endif
+                @if ($editproduct === null)
+                <div class="item__form-input-close">
                   <div id="category_sequence">{{ $product->quantity }}</div>
                   <span>Product Quantity</span>
               </div>
-              <div class="item__form-input-close item__form-long">
+                @else
+                <div class="item__form-input">
+                  <input type="number" min="0" wire:model.defer="prod.quantity" required>
+                  <span>Product Quantity</span>
+              </div>
+                @endif
+                @if ($editproduct === null)
+                <div class="item__form-input-close item__form-long">
                   <div id="category_short_description">{{ $product->short_description }}</div>
                   <span>Product Short Description</span>
               </div>
-              <div class="item__form-input-close item__form-long">
+                @else
+                <div class="item__form-input item__form-long">
+                  <input type="text" wire:model.defer="prod.short_description" required>
+                  <span>Product Short Description</span>
+              </div>
+                @endif
+                @if ($editproduct === null)
+                <div class="item__form-input-close item__form-long">
                   <div id="category_long_description">{{ $product->long_description }}</div>
                   <span>Product Long Description</span>
               </div>
-              <div class="item__form-input-close item__form-long">
+                @else
+                <div class="item__form-input item__form-long">
+                  <textarea wire:model.defer="long_description" required></textarea>
+                  <span>Product Long Description</span>
+              </div>
+                @endif
+                @if ($editproduct === null)
+                <div class="item__form-input-close item__form-long">
                   <div id="seo_title">{{ $product->seo_title }}</div>
                   <span>SEO Title</span>
               </div>
+                @else
+                <div class="item__form-input item__form-long">
+                  <input type="text" wire:model.defer="seo_title" required>
+                  <span>SEO Title</span>
+              </div>
+                @endif
               <div class="item__form-close">
                   <div>{{ $product->created_at }}</div>
                   <span>Create date / time</span>
@@ -96,7 +179,11 @@
                   <div>{{ $product->last_modified_by }}</div>
                   <span>Last modified by</span>
               </div>
-          </form>
+@if ($editproduct != null)
+<input class="item__form-btn item__form-long" wire:click.prevent="saveproduct()" type="button"  value="Save"
+id="Update">
+@endif
+            </div>
       </div>
       <div class="tabs__content display-f g-1">
           {{-- <livewire:related-media-category categoryId="{{ $category->id }}" /> --}}
