7cf9eee86d045f7beda7a0cae43c9441cfb41109
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-06-28 14:02:13 +0300

    Add model Specs

---
+++ app/Http/Controllers/SpecsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Specs;
+use App\Http\Requests\StoreSpecsRequest;
+use App\Http\Requests\UpdateSpecsRequest;
+
+class SpecsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSpecsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Specs $specs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Specs $specs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSpecsRequest $request, Specs $specs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Specs $specs)
+    {
+        //
+    }
+}
