4f47a1c5fa39a55d9c7ef95dc03902cb8411bd7c
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-07-04 17:20:08 +0300

    fixing bugs.2

--- resources/views/livewire/show-category.blade.php
+++ resources/views/livewire/show-category.blade.php
@@ -1,32 +1,32 @@
 <div>
-  @if (session()->has('message'))
-  <div class="alert__session liveAlert" id="alertevent">
-      <span class="alert__session-text">{!! session('message') !!}</span>
-      <button class="alert__session-btn" type="button" data-bs-dismiss="alert" aria-hidden="true">
-          <svg>
-              <line x1="18" y1="6" x2="6" y2="18"></line>
-              <line x1="6" y1="6" x2="18" y2="18"></line>
-          </svg>
-      </button>
-  </div>
-  <script>
-      const alertEvent = document.getElementById("alertevent");
-      header.style.marginBottom = '4rem';
-      alertEvent.style.opacity = '1';
+    @if (session()->has('message'))
+        <div class="alert__session liveAlert" id="alertevent">
+            <span class="alert__session-text">{!! session('message') !!}</span>
+            <button class="alert__session-btn" type="button" data-bs-dismiss="alert" aria-hidden="true">
+                <svg>
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+            </button>
+        </div>
+        <script>
+            const alertEvent = document.getElementById("alertevent");
+            header.style.marginBottom = '4rem';
+            alertEvent.style.opacity = '1';
 
-      setTimeout(function() {
-          alertEvent.style.opacity = '0';
-          setTimeout(function() {
-              alertEvent.remove();
-              header.style.marginBottom = '0';
-          }, 500);
-      }, 2000);
-  </script>
-@endif
+            setTimeout(function() {
+                alertEvent.style.opacity = '0';
+                setTimeout(function() {
+                    alertEvent.remove();
+                    header.style.marginBottom = '0';
+                }, 500);
+            }, 2000);
+        </script>
+    @endif
     <div class="item__header">
         <h1 class="item__header-title" id="title">Category - {{ $category->name }}</h1>
         <div class="item__header-buttons">
-            <a class="item__header-btn" href="{{ route('category') }}">Back</a>
+            <a class="item__header-btn" href="{{ route('category') }}">All Categories</a>
             <a class="item__header-btn" href="{{ route('newcategory') }}">New</a>
             @if ($editcategory === null)
                 <input class="item__header-btn" type="button" value="Edit" wire:click.prevent="editcategory()">
@@ -39,8 +39,6 @@
         </div>
     </div>
 
-
-
     <div class="tab">
         <div class="tabs">
             <h3 class="tabs__page active">Details</h3>
@@ -48,104 +46,101 @@
         </div>
         <div class="tab__list">
             <div class="tabs__content active" id="Details">
-                    <div class="item__form">
-                      @if ($editcategory === null)
-                      <div class="item__form-input-close">
-                        <div>{{ $category->name }}</div>
-                        <span>Category Name</span>
-                    </div>
-                  @else
-                  <div class="item__form-input">
-                    <input type="text" wire:model.defer="cat.name">
-                    <span> Name</span>
-                </div>
-                  @endif
-                  @if ($editcategory === null)
-                  <div class="item__form-input-close">
-                    <div>{{ $category->parrent }}</div>
-                    <span>Category Parrent</span>
-                </div>
-                  @else
-                  <div class="item__form-input">
-                    <select wire:model.defer="cat.parrent">
-                        <option>Select a parrent</option>
-                        @foreach ($categories as $category_name)
-                            <option value="{{ $category_name }}">{{ $category_name }}</option>
-                        @endforeach
-                    </select>
-                    <span>Parent</span>
-                </div>
-                  @endif
+                <div class="item__form">
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close">
+                            <div>{{ $category->name }}</div>
+                            <span>Category Name</span>
+                        </div>
+                    @else
+                        <div class="item__form-input">
+                            <input type="text" wire:model.defer="cat.name">
+                            <span> Name</span>
+                        </div>
+                    @endif
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close">
+                            <div>{{ $category->parrent }}</div>
+                            <span>Category Parrent</span>
+                        </div>
+                    @else
+                        <div class="item__form-input">
+                            <select wire:model.defer="cat.parrent">
+                                <option>Select a parrent</option>
+                                @foreach ($categories as $category_name)
+                                    <option value="{{ $category_name }}">{{ $category_name }}</option>
+                                @endforeach
+                            </select>
+                            <span>Parent</span>
+                        </div>
+                    @endif
 
-                  @if ($editcategory === null)
-                  <div class="item__form-input-close">
-                    <div>{{ $category->start_date }}</div>
-                    <span for="start_date">Category Start Date</span>
-                </div>
-              @else
-              <div class="item__form-input">
-                <input type="date" wire:model.defer="cat.start_date">
-                <span>Start Date</span>
-            </div>
-              @endif
-              @if ($editcategory === null)
-              <div class="item__form-input-close">
-                <div>{{ $category->end_date }}</div>
-                <span>Category End Date </span>
-            </div>
-              @else
-              <div class="item__form-input">
-                <input type="date" wire:model.defer="cat.end_date">
-                <span>End Date</span>
-            </div>
-              @endif
-              @if ($editcategory === null)
-              <div class="item__form-input-close">
-                <div>{{ $category->sequence }}</div>
-                <span>Category Sequence</span>
-            </div>
-              @else
-              <div class="item__form-input">
-                <input type="number" min="0" wire:model.defer="cat.sequence">
-                <span>Sequence</span>
-            </div>
-              @endif
-              @if ($editcategory === null)
-              <div class="item__form-input-close item__form-long">
-                <div>{{ $category->short_description }}</div>
-                <span>Category Short Description</span>
-            </div>
-              @else
-              <div class="item__form-input item__form-long">
-                <input type="text" wire:model.defer="cat.short_description">
-                <span>Short Description</span>
-            </div>
-              @endif
-              @if ($editcategory === null)
-              <div class="item__form-input-close item__form-long">
-                <div>{{ $category->long_description }}</div>
-                <span>Category Long Description</span>
-            </div>
-              @else
-              <div class="item__form-input item__form-long">
-                <textarea wire:model.defer="cat.long_description"></textarea>
-                <span>Long Description</span>
-            </div>
-              @endif
-              @if ($editcategory === null)
-              <div class="item__form-input-close item__form-long">
-                <div>{{ $category->seo_title }}</div>
-                <span>SEO Title</span>
-            </div>
-              @else
-              <div class="item__form-input item__form-long">
-                <input type="text" wire:model.defer="cat.seo_title">
-                <span>SEO Title</span>
-            </div>
-              @endif
-
-
-
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close">
+                            <div>{{ $category->start_date }}</div>
+                            <span for="start_date">Category Start Date</span>
+                        </div>
+                    @else
+                        <div class="item__form-input">
+                            <input type="date" wire:model.defer="cat.start_date">
+                            <span>Start Date</span>
+                        </div>
+                    @endif
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close">
+                            <div>{{ $category->end_date }}</div>
+                            <span>Category End Date </span>
+                        </div>
+                    @else
+                        <div class="item__form-input">
+                            <input type="date" wire:model.defer="cat.end_date">
+                            <span>End Date</span>
+                        </div>
+                    @endif
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close">
+                            <div>{{ $category->sequence }}</div>
+                            <span>Category Sequence</span>
+                        </div>
+                    @else
+                        <div class="item__form-input">
+                            <input type="number" min="0" wire:model.defer="cat.sequence">
+                            <span>Sequence</span>
+                        </div>
+                    @endif
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close item__form-long">
+                            <div>{{ $category->short_description }}</div>
+                            <span>Category Short Description</span>
+                        </div>
+                    @else
+                        <div class="item__form-input item__form-long">
+                            <input type="text" wire:model.defer="cat.short_description">
+                            <span>Short Description</span>
+                        </div>
+                    @endif
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close item__form-long">
+                            <div>{{ $category->long_description }}</div>
+                            <span>Category Long Description</span>
+                        </div>
+                    @else
+                        <div class="item__form-input item__form-long">
+                            <textarea wire:model.defer="cat.long_description"></textarea>
+                            <span>Long Description</span>
+                        </div>
+                    @endif
+                    @if ($editcategory === null)
+                        <div class="item__form-input-close item__form-long">
+                            <div>{{ $category->seo_title }}</div>
+                            <span>SEO Title</span>
+                        </div>
+                    @else
+                        <div class="item__form-input item__form-long">
+                            <input type="text" wire:model.defer="cat.seo_title">
+                            <span>SEO Title</span>
+                        </div>
+                    @endif
 
                     <div class="item__form-close">
                         <div>{{ $category->created_at }}</div>
@@ -164,10 +159,10 @@
                         <span>Last modified by</span>
                     </div>
                     @if ($editcategory != null)
-                        <input class="item__form-btn item__form-long" wire:click.prevent="savecategory()"
-                            type="button" value="Save">
+                        <input class="item__form-btn item__form-long" wire:click.prevent="savecategory()" type="button"
+                            value="Save">
                     @endif
-                  </div>
+                </div>
             </div>
             <div class="tabs__content display-f g-1">
                 <livewire:related-media-category categoryId="{{ $category->id }}" />
@@ -175,25 +170,25 @@
                 <livewire:related-product-category categoryId="{{ $category->id }}" />
             </div>
 
-    </div>
-    <div class="modal" id="confirmationmodalitem">
-      <div class="modal-content">
-          <h1 class="modal-content-title">
-              {{ __('Are you sure to delete this record?') }}
-          </h1>
-          <input wire:click.prevent="deleteSingleRecord()" class="modal-content-btn submit" type="button"
-              value="Confirm">
-          <input class="modal-content-btn delete" type="button"
-              onclick="document.getElementById('confirmationmodalitem').style.display='none'" value="Cancel">
+        </div>
+        <div class="modal" id="confirmationmodalitem">
+            <div class="modal-content">
+                <h1 class="modal-content-title">
+                    {{ __('Are you sure to delete this record?') }}
+                </h1>
+                <input wire:click.prevent="deleteSingleRecord()" class="modal-content-btn submit" type="button"
+                    value="Confirm">
+                <input class="modal-content-btn delete" type="button"
+                    onclick="document.getElementById('confirmationmodalitem').style.display='none'" value="Cancel">
 
-          <span class="modal-content-btn delete"
-              onclick="document.getElementById('confirmationmodalitem').style.display='none'">
-              <svg>
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
-          </span>
-      </div>
-  </div>
-</div>
+                <span class="modal-content-btn delete"
+                    onclick="document.getElementById('confirmationmodalitem').style.display='none'">
+                    <svg>
+                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                </span>
+            </div>
+        </div>
+    </div>
 </div>
