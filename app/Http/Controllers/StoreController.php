3c11abfef1a23b23318e6b67593ec9bbe01bb61b
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-07-11 12:10:09 +0300

    adaugare storefront

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Store;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStoreRequest;
+use App\Http\Requests\UpdateStoreRequest;
+
+class StoreController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return view('store.home');
+  }
+  public function cart()
+  {
+    return view('store.cart');
+  }
+  public function order()
+  {
+    return view('store.order');
+  }
+  public function checking()
+  {
+    return view('store.checking');
+  }
+  public function complete()
+  {
+    return view('store.complete');
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreStoreRequest $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Store $store)
+  {
+    return view('store.product');
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Store $store)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateStoreRequest $request, Store $store)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Store $store)
+  {
+    //
+  }
+}
---
+++ resources/views/store/product.blade.php
@@ -0,0 +1,185 @@
+<x-store-head />
+<x-store-header />
+<main>
+    <section class="container">
+        <div class="product">
+            <div class="product__preview">
+                <div class="product__image">
+                    <button class="product__image-prev">
+                        <svg>
+                            <polyline points="15 18 9 12 15 6"></polyline>
+                        </svg>
+                    </button>
+                    <img class="thumbnail-active" src="/images/store/bottle1.png" alt="Product Image" id="openModal">
+                    <button class="product__image-next">
+                        <svg>
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                    </button>
+                </div>
+                <div class="product__nails">
+                    <img class="thumbnail" src="/images/store/bottle1.png" alt="Thumbnail 1">
+                    <img class="thumbnail" src="/images/store/bottle2.png" alt="Thumbnail 2">
+                    <img class="thumbnail" src="/images/store/bottle3.png" alt="Thumbnail 3">
+                </div>
+                <div class="product__modal" id="modal">
+                    <button class="product__modal-prev">
+                        <svg>
+                            <polyline points="15 18 9 12 15 6"></polyline>
+                        </svg>
+                    </button>
+                    <img class="product__modal-img" src="img/bottle2.png" alt="Product Image">
+                    <button class="product__modal-next">
+                        <svg>
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="product__info">
+                <h1>"HydraMax 24: The Ultimate Quenching Companion"</h1>
+                <p>
+                    Stay hydrated on-the-go with HydraMax 24, the perfect companion for active individuals. This
+                    24-ounce water bottle combines stylish design with advanced insulation technology to keep
+                    your beverages at the ideal temperature for hours. Whether you're hitting the gym, hiking, or
+                    simply running errands, HydraMax 24 ensures you stay refreshed and energized throughout the day.
+                </p>
+                <span>50.00€</span>
+            </div>
+            <div class="product__count">
+                <button id="countDecrease">
+                    <svg>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                </button>
+                <input type="number" name="count" id="count" min="1" value="1">
+                <button id="countIncrease">
+                    <svg>
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                </button>
+            </div>
+            <div class="product__buttons">
+                <button class="product__btn">Add to cart</button>
+                <button class="product__btn">
+                    <svg>
+                        <path
+                            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
+                        </path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+        <div class="tab">
+            <div class="tab__header">
+                <button class="tab__header--btn">Description</button>
+                <button class="tab__header--btn">Details</button>
+            </div>
+            <div class="tab__content">
+                <div class="tab__pane active">
+                    <p class="product__description-long">
+                        Introducing the HydraMax 24: The Ultimate Quenching Companion for Active Lifestyles
+                        <br>
+                        The HydraMax 24 is not just an ordinary water bottle; it's a game-changer for those who
+                        value staying hydrated while leading an active lifestyle. With its sleek and ergonomic
+                        design, this 24-ounce water bottle is meticulously crafted to be your reliable companion
+                        wherever your adventures take you.
+                        <br>
+                        One of the standout features of the HydraMax 24 is its advanced insulation technology.
+                        Designed to keep your beverages at the perfect temperature, it ensures your drinks stay
+                        refreshingly cold on scorching summer days and pleasantly warm during chilly winter
+                        outings. No matter the weather, you can count on HydraMax 24 to deliver a satisfying sip
+                        every time.
+                        <br>
+                        Crafted with durability in mind, the HydraMax 24 is built to withstand the rigors of
+                        your active endeavors. Made from high-quality, BPA-free materials, it guarantees a
+                        long-lasting and safe hydration experience. Its leak-proof construction provides peace
+                        of mind, allowing you to toss it into your bag without worry, while the convenient carry
+                        handle ensures easy transportation wherever you go.
+                        <br>
+                        But the HydraMax 24 doesn't stop at just functionality—it's a statement of style. The
+                        sleek and modern design, available in a range of vibrant colors and patterns, ensures
+                        that you can express your personal taste while staying hydrated. It effortlessly
+                        complements any outfit or gear, making it a fashion-forward accessory for fitness
+                        enthusiasts, outdoor adventurers, and busy professionals alike.
+                    </p>
+                </div>
+                <div class="tab__pane">
+                    <div class="table__wrapper">
+                        <table class="table__info">
+                            <thead>
+                                <tr>
+                                    <th>Specification </th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Specification</td>
+                                    <td>Description</td>
+                                </tr>
+                                <tr>
+                                    <td>Model</td>
+                                    <td>HydraMax 24</td>
+                                </tr>
+                                <tr>
+                                    <td>Capacity</td>
+                                    <td>24 ounces (710 ml)</td>
+                                </tr>
+                                <tr>
+                                    <td>Material</td>
+                                    <td>BPA-free, high-quality materials</td>
+                                </tr>
+                                <tr>
+                                    <td>Insulation</td>
+                                    <td>Advanced insulation technology for temperature retention</td>
+                                </tr>
+                                <tr>
+                                    <td>Leak-proof</td>
+                                    <td>Yes</td>
+                                </tr>
+                                <tr>
+                                    <td>Carry handle</td>
+                                    <td>Yes</td>
+                                </tr>
+                                <tr>
+                                    <td>Design</td>
+                                    <td>Sleek and ergonomic</td>
+                                </tr>
+                                <tr>
+                                    <td>Colors</td>
+                                    <td>Multiple vibrant options available</td>
+                                </tr>
+                                <tr>
+                                    <td>Dimensions</td>
+                                    <td>Height: 9.5 inches (24 cm)<br> Diameter: 2.75 inches (7 cm)</td>
+                                </tr>
+                                <tr>
+                                    <td>Weight</td>
+                                    <td>Approximately 0.45 pounds (200 grams)</td>
+                                </tr>
+                                <tr>
+                                    <td>Suitable for</td>
+                                    <td>Active lifestyles, sports, outdoor activities, daily use</td>
+                                </tr>
+                                <tr>
+                                    <td>Safety</td>
+                                    <td>BPA-free and non-toxic</td>
+                                </tr>
+                                <tr>
+                                    <td>Warranty</td>
+                                    <td>Manufacturer's warranty included</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+</main>
+<x-store-footer />
