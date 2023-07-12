0239e9b69a68dee0c0337387d6eee81874ea7049
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-07-12 16:45:14 +0300

    lazy load product on store

--- app/Http/Livewire/StoreProducts.php
+++ app/Http/Livewire/StoreProducts.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class StoreProducts extends Component
 {
-    public function render()
-    {
-        return view('livewire.store-products');
-    }
+
+  public $totalRecords;
+  public $loadAmount = 9;
+
+  public function loadMore()
+  {
+    $this->loadAmount += 10;
+  }
+
+  public function mount()
+  {
+    $this->totalRecords = Product::count();
+  }
+  public function render()
+  {
+    return view('livewire.store-products', [
+      'products' => $this->products
+    ]);
+  }
+  public function getProductsProperty()
+  {
+    return $this->productsQuery->limit($this->loadAmount)->get();
+  }
+  public function getProductsQueryProperty()
+  {
+    return Product::orderBy('created_at', 'desc');
+  }
 }
--- resources/views/components/store-head.blade.php
+++ resources/views/components/store-head.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ __('Store') }}</title>
     <link rel="stylesheet" href="/dist/css/store.css">
+
+    @livewireStyles
 </head>
 
 <body>
