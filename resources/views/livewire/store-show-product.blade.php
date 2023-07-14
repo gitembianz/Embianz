9aaff9eba1d666e8f926f359cf3647e37beac3f3
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-07-14 16:58:21 +0300

    updated view products image

--- resources/views/livewire/store-show-product.blade.php
+++ resources/views/livewire/store-show-product.blade.php
@@ -2,18 +2,46 @@
     <div class="product">
         <div class="product__preview">
             <div class="product__image">
-                <button class="product__image-prev">
-                    <svg>
-                        <polyline points="15 18 9 12 15 6"></polyline>
-                    </svg>
-                </button>
-                <img class="thumbnail-active" src="/images/store/bottle1.png" alt="Product Image" id="openModal">
+                @if ($medias->first())
+                    <button class="product__image-prev">
+                        <svg>
+                            <polyline points="15 18 9 12 15 6"></polyline>
+                        </svg>
+                    </button>
+                    <?php
+                    $foundMedia = false; // Variable to track if matching media is found
+                    $mediaPath = null; // Variable to store the media path
+                    ?>
+                    @foreach ($medias as $media)
+                        @if ($media->location->location == 'main')
+                            <?php
+                            $foundMedia = true;
+                            $mediaPath = $media->extrenal ? $media->path : "/{$media->path}{$media->name}";
+                            ?>
+                        @break
+
+                        // Exit the inner loop once a matching media is found
+                    @endif
+                @endforeach
+                @if ($foundMedia)
+                    <img class="thumbnail-active" src="{{ $mediaPath }}" alt="Product Image" id="openModal">
+                @else
+                    <img class="thumbnail-active" src="/images/store/default/product.png" alt="Product Image"
+                        id="openModal">
+                @endif
+
                 <button class="product__image-next">
                     <svg>
                         <polyline points="9 18 15 12 9 6"></polyline>
                     </svg>
                 </button>
-            </div>
+            @else
+                <img class="thumbnail-active" src="/images/store/default/product.png" alt="Product Image"
+                    id="openModal">
+            @endif
+
+        </div>
+        @if ($medias->first() && count($medias) > 1)
             <div class="product__nails">
                 <img class="thumbnail" src="/images/store/bottle1.png" alt="Thumbnail 1">
                 <img class="thumbnail" src="/images/store/bottle-img2.webp" alt="Thumbnail 2">
@@ -25,7 +53,9 @@
                 <img class="thumbnail" src="/images/store/bottle2.png" alt="Thumbnail 2">
                 <img class="thumbnail" src="/images/store/bottle3.png" alt="Thumbnail 3">
             </div>
-            <div class="product__modal" id="modal">
+        @else
+        @endif
+        {{-- <div class="product__modal" id="modal">
                 <div class="slideshow">
                     <!-- Full-width images with number and caption text -->
                     <div class="slideshow--slides">
@@ -69,88 +99,88 @@
                         </svg>
                     </button>
                 </div>
-            </div>
-        </div>
-        <div class="product__info">
-            <h1>{{ $product->name }}</h1>
-            {{-- {{ $medias->first() }} --}}
-            <p>{{ $product->short_description }}</p>
-            <div class="product__price">
-                <div class="product__count">
-                    <button id="countDecrease">
-                        <svg>
-                            <line x1="5" y1="12" x2="19" y2="12"></line>
-                        </svg>
-                    </button>
-                    <input type="number" name="count" id="count" min="1" value="1">
-                    <button id="countIncrease">
-                        <svg>
-                            <line x1="12" y1="5" x2="12" y2="19"></line>
-                            <line x1="5" y1="12" x2="19" y2="12"></line>
-                        </svg>
-                    </button>
-                </div>
-                <h3>
-                    @if ($product->product_prices->first() !== null)
-                        {{ $product->product_prices->first()->value }}
-                        {{ $product->product_prices->first()->pricelist->currency->first()->name }}
-                    @else
-                        no price
-                    @endif
-                </h3>
-            </div>
-            <div class="product__buttons">
-                <button class="product__btn">Add to cart</button>
-                <button class="product__btn">
+            </div> --}}
+    </div>
+    <div class="product__info">
+        <h1>{{ $product->name }}</h1>
+
+        <p>{{ $product->short_description }}</p>
+        <div class="product__price">
+            <div class="product__count">
+                <button id="countDecrease">
                     <svg>
-                        <path
-                            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
-                        </path>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                </button>
+                <input type="number" name="count" id="count" min="1" value="1">
+                <button id="countIncrease">
+                    <svg>
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
                     </svg>
                 </button>
             </div>
+            <h3>
+                @if ($product->product_prices->first() !== null)
+                    {{ $product->product_prices->first()->value }}
+                    {{ $product->product_prices->first()->pricelist->currency->first()->name }}
+                @else
+                    no price
+                @endif
+            </h3>
+        </div>
+        <div class="product__buttons">
+            <button class="product__btn">Add to cart</button>
+            <button class="product__btn">
+                <svg>
+                    <path
+                        d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
+                    </path>
+                </svg>
+            </button>
         </div>
     </div>
+</div>
 
-    <div class="tab">
-        <div class="tab__header">
-            <button class="tab__header--btn @if ($activeTab === 0) active @endif"
-                wire:click="switchTab(0)">Description</button>
-            <button class="tab__header--btn @if ($activeTab === 1) active @endif"
-                wire:click="switchTab(1)">Details</button>
+<div class="tab">
+    <div class="tab__header">
+        <button class="tab__header--btn @if ($activeTab === 0) active @endif"
+            wire:click="switchTab(0)">Description</button>
+        <button class="tab__header--btn @if ($activeTab === 1) active @endif"
+            wire:click="switchTab(1)">Details</button>
+    </div>
+    <div class="tab__content">
+        <div class="tab__pane @if ($activeTab === 0) active @endif">
+            <p>{{ $product->long_description }}</p>
         </div>
-        <div class="tab__content">
-            <div class="tab__pane @if ($activeTab === 0) active @endif">
-                <p>{{ $product->long_description }}</p>
-            </div>
-            <div class="tab__pane @if ($activeTab === 1) active @endif">
-                <div class="table__wrapper">
-                    <table class="table__info">
-                        <thead>
-                            <tr>
-                                <th>Specification </th>
-                                <th>Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if ($product->product_specs->first() !== null)
-                                @foreach ($product->product_specs as $spec)
-                                    <tr>
-                                        <td>{{ $spec->spec->name }}</td>
-                                        <td>{{ $spec->value }}</td>
-                                    </tr>
-                                @endforeach
-                            @else
+        <div class="tab__pane @if ($activeTab === 1) active @endif">
+            <div class="table__wrapper">
+                <table class="table__info">
+                    <thead>
+                        <tr>
+                            <th>Specification </th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($product->product_specs->first() !== null)
+                            @foreach ($product->product_specs as $spec)
                                 <tr>
-                                    <td colspan="2">No Specs for this product</td>
+                                    <td>{{ $spec->spec->name }}</td>
+                                    <td>{{ $spec->value }}</td>
                                 </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="2">No Specs for this product</td>
+                            </tr>
 
-                            @endif
-                        </tbody>
-                    </table>
-                </div>
-
+                        @endif
+                    </tbody>
+                </table>
             </div>
+
         </div>
     </div>
+</div>
 </div>
