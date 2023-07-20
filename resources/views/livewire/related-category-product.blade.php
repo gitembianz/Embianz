f0748a06e7a942fab4e44d1a8a6b3eafd630d116
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-07-20 15:47:54 +0300

    delayed loading

--- resources/views/livewire/related-category-product.blade.php
+++ resources/views/livewire/related-category-product.blade.php
@@ -23,7 +23,7 @@
             }, 2000);
         </script>
     @endif
-    <div wire:loading>
+    <div wire:loading.delay>
         <div class="modal" style="display:flex; z-index: 99999;">
             <div class="loader"></div>
         </div>
