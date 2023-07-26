c73ef509011720de8326c1b6f0abefab06ae748e
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-07-26 12:09:31 +0300

    fixing

--- resources/views/livewire/categoriestable.blade.php
+++ resources/views/livewire/categoriestable.blade.php
@@ -1,38 +1,16 @@
 <div>
     {{-- sesssion html --}}
     @if (session()->has('message'))
-        {{-- <div class="alert__session liveAlert" id="alertevent">
-            <span class="alert__session-text">{!! session('message') !!}</span>
-            <button class="alert__session-btn" type="button" data-bs-dismiss="alert" aria-hidden="true">
-                <svg>
-                    <line x1="18" y1="6" x2="6" y2="18"></line>
-                    <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg>
-            </button>
-        </div>
-        <script>
-            const alertEvent = document.getElementById("alertevent");
-            header.style.marginBottom = '4rem';
-            alertEvent.style.opacity = '1';
-            setTimeout(function() {
-                alertEvent.style.opacity = '0';
-                setTimeout(function() {
-                    alertEvent.remove();
-                    header.style.marginBottom = '0';
-                }, 500);
-            }, 2000);
-        </script> --}}
+        {{-- aici se afla notificarea noua --}}
         <div class="notifications">
-            <div class="toast">
+            <div class="toast" id="alertevent">
+                <img src="images/dashboard/succes.svg" alt="succes">
                 <div class="toast__text">
-                    <h3>Notification Name</h3>
-                    <span>Notification Description</span>
+                    <h3>Success</h3>
+                    <span>{!! session('message') !!}</span>
                 </div>
-                <button>
-                    <svg>
-                        <line x1="18" y1="6" x2="6" y2="18"></line>
-                        <line x1="6" y1="6" x2="18" y2="18"></line>
-                    </svg>
+                <button type="button" data-bs-dismiss="alert" aria-hidden="true">
+                    Close
                 </button>
             </div>
         </div>
