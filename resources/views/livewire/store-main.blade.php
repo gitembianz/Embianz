f73c9644051b1544e43ef86d3f50e74825b17780
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-07-26 15:57:41 +0300

    optimizing

--- resources/views/livewire/store-main.blade.php
+++ resources/views/livewire/store-main.blade.php
@@ -1,6 +1,6 @@
 <div class="home__container" id="homeContainer">
     <div id="home__slide">
-        <div class="home__item" style="background-image: url(images/store/background-bottle1.jpeg);">
+        <div class="home__item" style="background-image: url(images/store/background-bottle1.webp);">
             <div class="home__content">
                 <div class="container home__content--flex">
                     <h2>ASTER - 500 ML</h2>
@@ -16,7 +16,7 @@
                 </div>
             </div>
         </div>
-        <div class="home__item" style="background-image:  url(images/store/background-bottle2.jpeg);">
+        <div class="home__item" style="background-image:  url(images/store/background-bottle2.webp);">
             <div class="home__content">
                 <div class="container home__content--flex">
                     <h2>BEGONIA - 500 ML</h2>
@@ -32,7 +32,7 @@
                 </div>
             </div>
         </div>
-        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle3.jpeg);">
+        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle3.webp);">
             <div class=" home__content">
                 <div class="container home__content--flex">
                     <h2>EBONY ROSE - 500 ML</h2>
@@ -47,7 +47,7 @@
                 </div>
             </div>
         </div>
-        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle4.jpeg);">
+        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle4.webp);">
             <div class=" home__content">
                 <div class="container home__content--flex">
                     <h2>GRACE - 500 ML</h2>
@@ -62,7 +62,7 @@
                 </div>
             </div>
         </div>
-        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle5.jpeg);">
+        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle5.webp);">
             <div class=" home__content">
                 <div class="container home__content--flex">
                     <h2>LITTLE BUDS - 500 ML</h2>
@@ -79,7 +79,7 @@
                 </div>
             </div>
         </div>
-        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle6.jpeg);">
+        <div class="home__item home__this" style="background-image:  url(images/store/background-bottle6.webp);">
             <div class=" home__content">
                 <div class="container home__content--flex">
                     <h2>LOTUS - 500 ML</h2>
@@ -122,7 +122,7 @@
         </button>
         <ul class="card-carousel">
             <li class="card">
-                <img src="images/store/bottle1.png" alt="img" draggable="false">
+                <img src="images/store/bottle1.webp" alt="img" draggable="false">
                 <h2>EcoPure</h2>
                 <p>Stay hydrated with the EcoPure water bottle by 24bottles.</p>
                 <span>Eco-friendly, BPA-free, Reusable</span>
@@ -130,7 +130,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/bottle2.png" alt="img" draggable="false">
+                <img src="images/store/bottle2.webp" alt="img" draggable="false">
                 <h2>Urban Oasis</h2>
                 <p>The Urban Oasis water bottle is perfect for on-the-go hydration.</p>
                 <span>Travel, Leak-proof, Stainless steel</span>
@@ -141,7 +141,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/bottle3.png" alt="img" draggable="false">
+                <img src="images/store/bottle3.webp" alt="img" draggable="false">
                 <h2>AquaLite</h2>
                 <p>The AquaLite water bottle offers lightweight and durable design.</p>
                 <span>Sports, Gym, Active lifestyle</span>
@@ -149,7 +149,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/background-bottle1.jpeg" alt="img" draggable="false">
+                <img src="images/store/background-bottle1.webp" alt="img" draggable="false">
                 <h2>Pure Flow</h2>
                 <p>Embrace pure hydration with the sleek Pure Flow water bottle.</p>
                 <span>Minimalist, BPA-free, Sustainable</span>
@@ -157,7 +157,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/background-bottle2.jpeg" alt="img" draggable="false">
+                <img src="images/store/background-bottle2.webp" alt="img" draggable="false">
                 <h2>Oasis Pro</h2>
                 <p>The Oasis Pro water bottle keeps your beverages at the perfect temperature.</p>
                 <span>Insulated, Outdoor, Hot and cold</span>
@@ -165,7 +165,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/background-bottle3.jpeg" alt="img" draggable="false">
+                <img src="images/store/background-bottle3.webp" alt="img" draggable="false">
                 <h2>Terra Essence</h2>
                 <p>Experience nature with the Terra Essence water bottle.</p>
                 <span>Nature-inspired, Sustainable</span>
@@ -173,7 +173,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/background-bottle4.jpeg" alt="img" draggable="false">
+                <img src="images/store/background-bottle4.webp" alt="img" draggable="false">
                 <h2>AquaWave</h2>
                 <p>Ride the waves of hydration with the AquaWave water bottle.</p>
                 <span>Surfing, Beach, Adventure</span>
@@ -181,7 +181,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/background-bottle5.jpeg" alt="img" draggable="false">
+                <img src="images/store/background-bottle5.webp" alt="img" draggable="false">
                 <h2>Urban Sip</h2>
                 <p>Sip in style with the Urban Sip water bottle.</p>
                 <span>Fashion, Trendy, Urban</span>
@@ -189,7 +189,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/background-bottle6.jpeg" alt="img" draggable="false">
+                <img src="images/store/background-bottle6.webp" alt="img" draggable="false">
                 <h2>Fresh Spring</h2>
                 <p>Quench your thirst with the Fresh Spring water bottle.</p>
                 <span>Refreshing, Nature, Hiking</span>
@@ -197,7 +197,7 @@
             </li>
 
             <li class="card">
-                <img src="images/store/bottle3.png" alt="img" draggable="false">
+                <img src="images/store/bottle3.webp" alt="img" draggable="false">
                 <h2>Summit Elite</h2>
                 <p>Reach new heights with the Summit Elite water bottle.</p>
                 <span>Mountaineering, Climbing, Outdoor</span>
@@ -211,7 +211,7 @@
         </button>
     </div>
 </div>
-<div class="home__video">
+{{-- <div class="home__video">
     <video autoplay loop muted playsinline defaultmuted preload="auto">
         <source src="images/store/myVideo.mp4" type="video/mp4">
         Your browser does not support HTML5 video.
@@ -222,105 +222,4 @@
         <span><q><i>20% din profitul nostru susține protecția mediului!</i></q></span>
         <img src="images/store/white-logo.svg" alt="logo">
     </div>
-</div>
-
-<script>
-    document.getElementById('home__prev').onclick = function() {
-        let lists = document.querySelectorAll('.home__item');
-        document.getElementById('home__slide').prepend(lists[lists.length - 1]);
-    }
-
-    document.getElementById('home__next').onclick = function() {
-        let lists = document.querySelectorAll('.home__item');
-        document.getElementById('home__slide').appendChild(lists[0]);
-    }
-</script>
-<script>
-    const wrapper = document.querySelector(".card-wrapper");
-    const carousel = document.querySelector(".card-carousel");
-    const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-    const arrowBtns = document.querySelectorAll(".card-button");
-    const carouselChildrens = [...carousel.children];
-
-    let isDragging = false,
-        isAutoPlay = true,
-        startX, startScrollLeft, timeoutId;
-
-    // Get the number of cards that can fit in the carousel at once
-    let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-
-    // Insert copies of the last few cards to beginning of carousel for infinite scrolling
-    carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-        carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-    });
-
-    // Insert copies of the first few cards to end of carousel for infinite scrolling
-    carouselChildrens.slice(0, cardPerView).forEach(card => {
-        carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-    });
-
-    // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-    carousel.classList.add("no-transition");
-    carousel.scrollLeft = carousel.offsetWidth;
-    carousel.classList.remove("no-transition");
-
-    // Add event listeners for the arrow buttons to scroll the carousel left and right
-    arrowBtns.forEach(btn => {
-        btn.addEventListener("click", () => {
-            carousel.scrollLeft += btn.id == "cardLeft" ? -firstCardWidth : firstCardWidth;
-        });
-    });
-
-    const dragStart = (e) => {
-        isDragging = true;
-        carousel.classList.add("dragging");
-        // Records the initial cursor and scroll position of the carousel
-        startX = e.pageX;
-        startScrollLeft = carousel.scrollLeft;
-    }
-
-    const dragging = (e) => {
-        if (!isDragging) return; // if isDragging is false return from here
-        // Updates the scroll position of the carousel based on the cursor movement
-        carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-    }
-
-    const dragStop = () => {
-        isDragging = false;
-        carousel.classList.remove("dragging");
-    }
-
-    const infiniteScroll = () => {
-        // If the carousel is at the beginning, scroll to the end
-        if (carousel.scrollLeft === 0) {
-            carousel.classList.add("no-transition");
-            carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-            carousel.classList.remove("no-transition");
-        }
-        // If the carousel is at the end, scroll to the beginning
-        else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-            carousel.classList.add("no-transition");
-            carousel.scrollLeft = carousel.offsetWidth;
-            carousel.classList.remove("no-transition");
-        }
-
-        // Clear existing timeout & start autoplay if mouse is not hovering over carousel
-        clearTimeout(timeoutId);
-        if (!wrapper.matches(":hover")) autoPlay();
-    }
-
-    const autoPlay = () => {
-        if (window.innerWidth < 800 || !isAutoPlay)
-            return; // Return if window is smaller than 800 or isAutoPlay is false
-        // Autoplay the carousel after every 2500 ms
-        timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-    }
-    autoPlay();
-
-    carousel.addEventListener("mousedown", dragStart);
-    carousel.addEventListener("mousemove", dragging);
-    document.addEventListener("mouseup", dragStop);
-    carousel.addEventListener("scroll", infiniteScroll);
-    wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-    wrapper.addEventListener("mouseleave", autoPlay);
-</script>
+</div> --}}
