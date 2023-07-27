58bd61b3b3d8120414f90b13fd32e852ec3476ba
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-07-27 11:16:22 +0300

    restore store cart

--- resources/views/components/store-header.blade.php
+++ resources/views/components/store-header.blade.php
@@ -109,7 +109,42 @@
                     </svg>
                 </button>
                 <ul class="cart__list" id="cartContent" aria-label="Cart Items">
-                    <!-- Cart items with proper labels and alternative text for images -->
+                    <li>
+                        <a href="/cart.html" class="cart__list--item">
+                            <h4 class="cart__list--name">Your cart</h4>
+                            <svg>
+                                <circle cx="9" cy="21" r="1"></circle>
+                                <circle cx="20" cy="21" r="1"></circle>
+                                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li><a class="cart__list--item" href="#">
+                            <img class="cart__list--img" src="/images/store/bottle1.webp"
+                                alt="Sustainable Sips: Reusable Bottles">
+                            <span class="cart__list--text">Sustainable Sips: Reusable Bottles</span>
+                            <span class="cart__list--much">x2</span>
+                            <span class="cart__list--much">20$</span>
+                            <button class="cart__list--delete">
+                                <svg>
+                                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                                </svg>
+                            </button>
+                        </a></li>
+                    <li><a class="cart__list--item" href="#">
+                            <img class="cart__list--img" src="/images/store/bottle2.webp"
+                                alt="Sustainable Sips: Reusable Bottles">
+                            <span class="cart__list--text">Sustainable Sips: Reusable Bottles</span>
+                            <span class="cart__list--much">x5</span>
+                            <span class="cart__list--much">40$</span>
+                            <button class="cart__list--delete">
+                                <svg>
+                                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                                </svg>
+                            </button>
+                        </a></li>
                 </ul>
             </div>
             <a href="#" class="heart" aria-label="Favorites">
