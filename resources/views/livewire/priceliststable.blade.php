82c6b6eb790c7641753756645c947e50e6321842
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-08-07 11:31:31 +0300

    ..

--- resources/views/livewire/priceliststable.blade.php
+++ resources/views/livewire/priceliststable.blade.php
@@ -273,9 +273,10 @@
                             </button>
                             <button class="delete" wire:click.prevent="confirmItemRemoval({{ $price->id }})">
                                 <svg>
-                                    <circle cx="12" cy="12" r="10"></circle>
-                                    <line x1="15" y1="9" x2="9" y2="15"></line>
-                                    <line x1="9" y1="9" x2="15" y2="15"></line>
+                                    <polyline points="3 6 5 6 21 6"></polyline>
+                                    <path
+                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                    </path>
                                 </svg>
                             </button>
                         @else
