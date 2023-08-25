4b6cc19d7b1ee57db469a04b7037173bee6ba091
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-08-25 17:41:06 +0300

    Product->related specs - a ramas doar massedit

--- resources/views/livewire/related-spec-product.blade.php
+++ resources/views/livewire/related-spec-product.blade.php
@@ -235,19 +235,19 @@
                 <div>
                     @if ($relatedspecs && count($relatedspecs) > 0)
                         {{-- delete single record --}}
-                        <div class="modal" id="confirmationmodal">
+                        <div class="modal" id="confirmationmodalspec">
                             <div class="modal-content">
                                 <h1 class="modal-content-title">
                                     {{ __('Are you sure to delete this record?') }}
                                 </h1>
                                 <input wire:click.prevent="deleteSingleRecord()" class="modal-content-btn submit"
-                                    type="button" value="Confirm" id="confirmLoad">
+                                    type="button" value="Confirm">
                                 <input class="modal-content-btn delete" type="button"
-                                    onclick="document.getElementById('confirmationmodal').style.display='none'"
+                                    onclick="document.getElementById('confirmationmodalspec').style.display='none'"
                                     value="Cancel">
 
                                 <span class="modal-content-btn delete"
-                                    onclick="document.getElementById('confirmationmodal').style.display='none'">
+                                    onclick="document.getElementById('confirmationmodalspec').style.display='none'">
                                     <svg>
                                         <line x1="18" y1="6" x2="6" y2="18">
                                         </line>
@@ -264,7 +264,7 @@
                                     {{ __('Are you sure to delete those records?') }}
                                 </h1>
                                 <input wire:click.prevent="deleteRecords()" class="modal-content-btn submit"
-                                    type="button" value="Confirm" id="confirmLoad">
+                                    type="button" value="Confirm">
                                 <input class="modal-content-btn delete" type="button"
                                     onclick="document.getElementById('confirmationmodalmultiple').style.display='none'"
                                     value="Cancel">
