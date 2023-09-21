0685efad7f13070e76bf2bca56216c26cc01a2eb
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-09-21 14:24:26 +0300

    Storefront-> toggle wislit on shoe product

--- resources/views/livewire/store-show-product.blade.php
+++ resources/views/livewire/store-show-product.blade.php
@@ -106,13 +106,21 @@
             </div>
             <div class="product__buttons">
                 <button class="product__btn">Add to cart</button>
-                <button class="product__btn">
+                <button class="product__item--heart @if ($product->wishlists->where('session_id', $session_id)->isNotEmpty()) active @endif"
+                    aria-label="add to favorites" wire:click="toggleWishlist({{ $product->id }})">
                     <svg>
                         <path
                             d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
                         </path>
                     </svg>
                 </button>
+                {{-- <button class="product__btn">
+                    <svg>
+                        <path
+                            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
+                        </path>
+                    </svg>
+                </button> --}}
             </div>
         </div>
     </div>
