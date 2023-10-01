bd3a1aeba2628a0ba08734c6b5dd1a013cd9b44c
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2023-10-01 22:07:04 +0300

    Storefront-> Order page in 3 steps working

--- app/Http/Livewire/StoreCart.php
+++ app/Http/Livewire/StoreCart.php
@@ -8,6 +8,8 @@ use App\Models\Voucher;
 use Livewire\Component;
 use App\Models\Wishlist;
 use App\Models\Cart_Item;
+use App\Models\Order;
+use App\Models\Order_Item;
 use Illuminate\Support\Facades\Session;
 
 class StoreCart extends Component
@@ -181,11 +183,31 @@ class StoreCart extends Component
     if ($this->new_price) {
       $cart->final_amount = $this->price;
       $cart->save();
-      dd($cart->final_amount);
     } else {
       $cart->final_amount = $cart->sum_amount + $this->deliverry;
       $cart->save();
-      dd($cart->final_amount);
     }
+    $order = Order::where('session_id', $this->session_id)->first();
+    if (!$order) {
+      Order::create([
+        'session_id' => $this->session_id,
+        'quantity_amount' => $cart->quantity_amount,
+        'sum_amount' => $cart->final_amount,
+        'status' => 'in progress',
+      ]);
+      $cartitems = Cart_Item::where('cart_id', $cart->id)->get();
+      if ($cartitems) {
+        $order = Order::where('session_id', $this->session_id)->first();
+        foreach ($cartitems as $item) {
+          Order_Item::create([
+            'order_id' => $order->id,
+            'product_id' => $item->product_id,
+            'price' => $item->price,
+            'quantity' => $item->quantity,
+          ]);
+        }
+      }
+    }
+    return redirect()->route('order');
   }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
   use HasFactory;
+  protected $fillable = ['session_id', 'quantity_amount', 'sum_amount', 'status'];
+
   public function orders()
   {
     return $this->hasMany(Order_Item::class, 'order_id');
