4071aa47754366da8a9f36e0f9a2147a075b2aff
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-11-24 14:49:42 +0200

    reffiling the safary screen on horizontal

--- resources/views/components/store-head.blade.php
+++ resources/views/components/store-head.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
         <title>{{ $title }}</title>
         <meta name="description"
             content="Ecosticle.ro is a website dedicated to providing eco-friendly products for a sustainable lifestyle. Shop our wide range of environmentally-friendly products including reusable items, zero-waste essentials, and more.">
