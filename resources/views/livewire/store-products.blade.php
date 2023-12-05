f1e68272af48922dcf89e54d06469d7d53bf5e9d
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2023-12-05 16:33:59 +0200

    Added disabled button  in store-products & store-show-product views

--- resources/views/livewire/store-products.blade.php
+++ resources/views/livewire/store-products.blade.php
@@ -142,6 +142,8 @@
 
                         @if ($price && $product->quantity != 0)
                             <a class="card-button" wire:click="addToCart({{ $product->id }})">Adauga in coș</a>
+                        @else
+                            <a class="card-button-disabled" onclick="handleClick()">Indisponibil</a>
                         @endif
 
                     </div>
