cdf723f0c07a7299a14d2215929555c2c4c8a42e
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-01-19 13:16:39 +0200

    Update app to the last verion by composer

--- vendor/laravel/framework/src/Illuminate/Bus/PendingBatch.php
+++ vendor/laravel/framework/src/Illuminate/Bus/PendingBatch.php
@@ -75,6 +75,31 @@ class PendingBatch
     }
 
     /**
+     * Add a callback to be executed after a job in the batch have executed successfully.
+     *
+     * @param  callable  $callback
+     * @return $this
+     */
+    public function progress($callback)
+    {
+        $this->options['progress'][] = $callback instanceof Closure
+            ? new SerializableClosure($callback)
+            : $callback;
+
+        return $this;
+    }
+
+    /**
+     * Get the "progress" callbacks that have been registered with the pending batch.
+     *
+     * @return array
+     */
+    public function progressCallbacks()
+    {
+        return $this->options['progress'] ?? [];
+    }
+
+    /**
      * Add a callback to be executed after all jobs in the batch have executed successfully.
      *
      * @param  callable  $callback
@@ -318,5 +343,27 @@ class PendingBatch
         $this->container->make(EventDispatcher::class)->dispatch(
             new BatchDispatched($batch)
         );
+    }
+
+    /**
+     * Dispatch the batch if the given truth test passes.
+     *
+     * @param  bool|\Closure  $boolean
+     * @return \Illuminate\Bus\Batch|null
+     */
+    public function dispatchIf($boolean)
+    {
+        return value($boolean) ? $this->dispatch() : null;
+    }
+
+    /**
+     * Dispatch the batch unless the given truth test passes.
+     *
+     * @param  bool|\Closure  $boolean
+     * @return \Illuminate\Bus\Batch|null
+     */
+    public function dispatchUnless($boolean)
+    {
+        return ! value($boolean) ? $this->dispatch() : null;
     }
 }
