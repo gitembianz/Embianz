cc478aa11c44320403bb9513f7c64298b415cca3
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-01-23 10:08:46 +0200

    Wishlist quantity button done

--- resources/views/livewire/wishlist-quantity.blade.php
+++ resources/views/livewire/wishlist-quantity.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- Be like water. --}}
+<div @if ($count != 0) class="header__count"
+@else
+    style ="dispaly:none !important" @endif
+    id="wishlistCount">
+    @if ($count != 0)
+        {{ $count }}
+    @endif
 </div>
