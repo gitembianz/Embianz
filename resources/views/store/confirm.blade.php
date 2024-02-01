d304f71be61170a54102c7c17f80326ec4c1ad87
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2024-02-01 16:30:51 +0200

    Confirm Page is done

--- resources/views/store/confirm.blade.php
+++ resources/views/store/confirm.blade.php
@@ -1,12 +1,36 @@
-<x-store-head :title='" Confirm message | "'  />
+<x-store-head :title='" Confirm message | "' />
 <x-store-header />
 <main>
-
-    <!-----------------------Contact Form----------------------->
-    <section class="contact container">
-
+    <!---------------------------------------------------------->
+    <!------------------------Breadcrumbs----------------------->
+    <section>
+        <div class="breadcrumbs container">
+            <a class="breadcrumbs__link" href="{{ url("/") }}">
+                Acasa
+            </a>
+        </div>
     </section>
-    <!---------------------End Contact Form--------------------->
+    <!----------------------End Breadcrumbs--------------------->
+    <!---------------------------------------------------------->
+    <!---------------------------------------------------------->
+    <!----------------------Section Header---------------------->
+    <section class="section__header container">
+        <h2 class="section__title">
+            Mulțumim pentru completarea formularului.
+        </h2>
+        <p class="section__text">
+            Veți fi redirecționat la pagina principală în 5 secunde...
+        </p>
+        <div class="loadingio-spinner-dual-ball-8eksdpgpyip">
+            <div class="ldio-sl0v29xbypi">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </section>
+    <!--------------------End Section Header-------------------->
+    <!---------------------------------------------------------->
     <script>
         // Așteaptă 5 secunde (5000 milisecunde) și apoi face redirect către o altă pagină
         setTimeout(function() {
