9bfac49340f11d5d8d2072027b7be84756c59f8a
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2024-02-07 11:58:30 +0200

    Fixing Special Code

--- resources/views/components/store-footer.blade.php
+++ resources/views/components/store-footer.blade.php
@@ -18,56 +18,46 @@
 <script type='text/javascript' src='https://service.force.com/embeddedservice/5.0/esw.min.js'></script>
 <script type='text/javascript'>
     var initESW = function(gslbBaseURL) {
-    embedded_svc.settings.displayHelpButton = true; //Or false
-    embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
+        embedded_svc.settings.displayHelpButton = true; //Or false
+        embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
 
-    //embedded_svc.settings.defaultMinimizedText = '...'; //(Defaults to
-    Chat with an Expert)
-    //embedded_svc.settings.disabledMinimizedText = '...'; //(Defaults to
-    Agent Offline)
+        //embedded_svc.settings.defaultMinimizedText = '...'; //(Defaults to Chat with an Expert)
+        //embedded_svc.settings.disabledMinimizedText = '...'; //(Defaults to Agent Offline)
 
-    //embedded_svc.settings.loadingText = ''; //(Defaults to Loading)
-    //embedded_svc.settings.storageDomain = 'yourdomain.com'; //(Sets the
-    domain
-    for your deployment so that visitors can navigate subdomains
-    during a chat session)
+        //embedded_svc.settings.loadingText = ''; //(Defaults to Loading)
+        //embedded_svc.settings.storageDomain = 'yourdomain.com'; //(Sets the domain for your deployment so that visitors can navigate subdomains during a chat session)
 
-    // Settings for Chat
-    //embedded_svc.settings.directToButtonRouting = function(prechatFormData) {
-    // Dynamically changes the button ID based on what the visitor enters
-    in the pre - chat form.
-    // Returns a valid button ID.
-    //};
-    //embedded_svc.settings.prepopulatedPrechatFields = {}; //Sets the
-    auto - population of pre - chat form fields
-    //embedded_svc.settings.fallbackRouting = []; //An array of button
-    IDs, user IDs, or userId_buttonId
-    //embedded_svc.settings.offlineSupportMinimizedText = '...';
-    //(Defaults to Contact Us)
+        // Settings for Chat
+        //embedded_svc.settings.directToButtonRouting = function(prechatFormData) {
+        // Dynamically changes the button ID based on what the visitor enters in the pre-chat form.
+        // Returns a valid button ID.
+        //};
+        //embedded_svc.settings.prepopulatedPrechatFields = {}; //Sets the auto-population of pre-chat form fields
+        //embedded_svc.settings.fallbackRouting = []; //An array of button IDs, user IDs, or userId_buttonId
+        //embedded_svc.settings.offlineSupportMinimizedText = '...'; //(Defaults to Contact Us)
 
-    embedded_svc.settings.enabledFeatures = ['LiveAgent'];
-    embedded_svc.settings.entryFeature = 'LiveAgent';
+        embedded_svc.settings.enabledFeatures = ['LiveAgent'];
+        embedded_svc.settings.entryFeature = 'LiveAgent';
 
-    embedded_svc.init(
-        'https://eztem.my.salesforce.com',
-        'https://eztem.my.site.com/liveAgentSetupFlow',
-        gslbBaseURL,
-        '00D09000008XPQu',
-        'Team_1', {
-            baseLiveAgentContentURL: 'https://c.la1-core1.sfdc-yzvdd4.salesforceliveagent.com/content',
-            deploymentId: '5729N0000000077',
-            buttonId: '5739N000000008U',
-            baseLiveAgentURL: 'https://d.la1-core1.sfdc-yzvdd4.salesforceliveagent.com/chat',
-            eswLiveAgentDevName: 'Team_1',
-            isOfflineSupportEnabled: false
-        }
-    );
+        embedded_svc.init(
+            'https://eztem.my.salesforce.com',
+            'https://eztem.my.site.com/liveAgentSetupFlow',
+            gslbBaseURL,
+            '00D09000008XPQu',
+            'Team_1', {
+                baseLiveAgentContentURL: 'https://c.la1-core1.sfdc-yzvdd4.salesforceliveagent.com/content',
+                deploymentId: '5729N0000000077',
+                buttonId: '5739N000000008U',
+                baseLiveAgentURL: 'https://d.la1-core1.sfdc-yzvdd4.salesforceliveagent.com/chat',
+                eswLiveAgentDevName: 'Team_1',
+                isOfflineSupportEnabled: true
+            }
+        );
     };
 
     if (!window.embedded_svc) {
         var s = document.createElement('script');
-        s.setAttribute('src',
-            'https://eztem.my.salesforce.com/embeddedservice/5.0/esw.min.js');
+        s.setAttribute('src', 'https://eztem.my.salesforce.com/embeddedservice/5.0/esw.min.js');
         s.onload = function() {
             initESW(null);
         };
--- resources/views/livewire/store-show-product.blade.php
+++ resources/views/livewire/store-show-product.blade.php
@@ -1,10 +1,10 @@
 <div id="store-show-product">
     <section>
         <div class="breadcrumbs container">
-            <a class="breadcrumbs__link" href="{{ url('/') }}">
+            <a class="breadcrumbs__link" href="{{ url("/") }}">
                 Acasa
             </a>
-            <a class="breadcrumbs__link" href="{{ url('/storeproducts') }}">
+            <a class="breadcrumbs__link" href="{{ url("/storeproducts") }}">
                 Produse
             </a>
             <a class="breadcrumbs__link">{{ $product->name }}</a>
@@ -17,10 +17,10 @@
 
                 <div class="product-slider__wrapper">
                     @if ($product->media->count() != 0)
-                        @foreach ($product->media->where('type', 'full') as $media)
+                        @foreach ($product->media->where("type", "full") as $media)
                             <div class="product-slider__slide">
                                 <img src="/{{ $media->path }}{{ $media->name }}" alt="{{ $media->path }}"
-                                    data-img-src="/{{ $product->media->where('type', 'original')->where('sequence', $media->sequence)->first()->path }}{{ $product->media->where('type', 'original')->where('sequence', $media->sequence)->first()->name }}">
+                                    data-img-src="/{{ $product->media->where("type", "original")->where("sequence", $media->sequence)->first()->path }}{{ $product->media->where("type", "original")->where("sequence", $media->sequence)->first()->name }}">
                             </div>
                         @endforeach
                     @else
@@ -64,10 +64,109 @@
         <!------------------ End Modal Product ---------------->
         <!------------------------------------------------------>
         <!----------------------- Product details ---------------------->
-        @livewire('product-details', ['product' => $product])
+        @livewire("product-details", ["product" => $product])
         <!--------------------- End Product details -------------------->
         <!------------------------------------------------------>
     </section>
+    <!---------------------------------------------------------->
+    <!------------------- Section Description ------------------>
+    <section>
+        <div class="section__header container">
+            <h2 class="section__title">Descoperă și alte opțiuni similare</h2>
+            <p class="section__text">
+                În căutarea perfectă? Explorează și alte propuneri care te-ar putea interesa.
+                Descoperă produse similare, perfecte pentru gusturile tale și nevoile tale. În continuare, vei găsi
+                opțiuni care completează gama noastră și care îți pot satisface preferințele. Alege cu încredere dintre
+                aceste alternative și găsește exact ceea ce cauți.
+            </p>
+        </div>
+    </section>
+    <!----------------- End Section Description ---------------->
+    <!---------------------------------------------------------->
+    <!---------------------- Slider Cards ---------------------->
+    <section>
+        <div class="card-slider container">
+            <div class="card-slider__wrapper">
+                <div class="card-slider__slide">
+                    <div class="card" role="listitem">
+                        <a href="#">
+                            <img class="card-image" src="/images/store/default/default300.webp" alt="something wrong">
+                        </a>
+                        <div class="card-info">
+                            <div class="card-text">
+                                <span>description</span>
+                            </div>
+                            <div class="card-text">
+                                <h3>Product Name</h3>
+                                <p class="card-price">
+                                    <span>
+                                        15.00$
+                                    </span>
+                                </p>
+                            </div>
+
+                            <a class="card-button-disabled">Indisponibil</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-slider__slide">
+                    <div class="card" role="listitem">
+                        <a href="#">
+                            <img class="card-image" src="/images/store/default/default300.webp" alt="something wrong">
+                        </a>
+                        <div class="card-info">
+                            <div class="card-text">
+                                <span>description</span>
+                            </div>
+                            <div class="card-text">
+                                <h3>Product Name</h3>
+                                <p class="card-price">
+                                    <span>
+                                        15.00$
+                                    </span>
+                                </p>
+                            </div>
+
+                            <a class="card-button-disabled">Indisponibil</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-slider__slide">
+                    <div class="card" role="listitem">
+                        <a href="#">
+                            <img class="card-image" src="/images/store/default/default300.webp" alt="something wrong">
+                        </a>
+                        <div class="card-info">
+                            <div class="card-text">
+                                <span>description</span>
+                            </div>
+                            <div class="card-text">
+                                <h3>Product Name</h3>
+                                <p class="card-price">
+                                    <span>
+                                        15.00$
+                                    </span>
+                                </p>
+                            </div>
+
+                            <a class="card-button-disabled">Indisponibil</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-slider__button prev">
+                <svg>
+                    <polyline points="15 18 9 12 15 6"></polyline>
+                </svg>
+            </div>
+            <div class="card-slider__button next">
+                <svg>
+                    <polyline points="9 18 15 12 9 6"></polyline>
+                </svg>
+            </div>
+        </div>
+    </section>
+    <!-------------------- End Slider Cards -------------------->
     <!---------------------------------------------------------->
     <!--------------------- support button --------------------->
     <x-help-button />
