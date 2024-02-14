8d5ac24434164ebcb5f2264fd13ddfd96ac3a073
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-02-14 08:48:58 +0200

    remove title tag from body

--- resources/views/components/store-head.blade.php
+++ resources/views/components/store-head.blade.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
-        <title>{{ $title }}</title>
-        <meta name="description"
-            content="Ecosticle.ro is a website dedicated to providing eco-friendly products for a sustainable lifestyle. Shop our wide range of environmentally-friendly products including reusable items, zero-waste essentials, and more.">
-        <link rel="stylesheet" type="text/css" href="/dist/css/loading-screen.css">
-        <link rel="icon" type="image/x-icon" href="/images/store/svg/noren-icon.svg">
-        <link rel="stylesheet" type="text/css" href="/dist/css/store.css">
-        @livewireStyles
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <title>{{ $title }}</title>
+    <meta name="description"
+        content="Ecosticle.ro is a website dedicated to providing eco-friendly products for a sustainable lifestyle. Shop our wide range of environmentally-friendly products including reusable items, zero-waste essentials, and more.">
+    <link rel="stylesheet" type="text/css" href="/dist/css/loading-screen.css">
+    <link rel="icon" type="image/x-icon" href="/images/store/svg/noren-icon.svg">
+    <link rel="stylesheet" type="text/css" href="/dist/css/store.css">
+    <link rel="canonical" href="{{ url('/') }}">
+    @livewireStyles
+</head>
 
-    <body id="body">
-        <title>{{ $title }}</title>
-        <!---------------------- Loading Logo ---------------------->
-        <div class="loading-logo" id="loadingLogo">
-            <img src="/images/store/svg/noren-black.svg" alt="logo-black">
-        </div>
-        <script src="/script/store/head.js"></script>
+<body id="body">
+    <!---------------------- Loading Logo ---------------------->
+    <div class="loading-logo" id="loadingLogo">
+        <img src="/images/store/svg/noren-black.svg" alt="logo-black">
+    </div>
+    <script src="/script/store/head.js"></script>
 
-        <!-------------------- End Loading Logo -------------------->
+    <!-------------------- End Loading Logo -------------------->
