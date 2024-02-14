17f1ba1324c0dc7b32b5cecaf7ebf7befc80e0df
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-02-14 11:29:53 +0200

    Optimizare flux/querys/resurse - main page si header

--- resources/views/livewire/cart-quantity.blade.php
+++ resources/views/livewire/cart-quantity.blade.php
@@ -1,11 +1,10 @@
 <div @if ($cart && $cart->quantity_amount != 0) class="header__count"
 @else
-    style ="dispaly:none !important" @endif
+    style="display:none !important" @endif
     id="cartCount">
     <span>
         @if ($cart->quantity_amount != 0)
             {{ $cart->quantity_amount }}
         @endif
     </span>
-
 </div>
--- resources/views/livewire/store-main.blade.php
+++ resources/views/livewire/store-main.blade.php
@@ -92,7 +92,7 @@
                                     $price = null;
                                     $discount = false;
                                     
-                                    if ($product->product_prices->count() != 0) {
+                                    if (!$product->product_prices->isEmpty()) {
                                         $price = number_format($product->product_prices->first()->value, 2, ',', '.');
                                         $discount = $product->product_prices->first()->discount != 0 ? true : false;
                                     }
