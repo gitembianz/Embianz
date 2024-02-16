d77ef9f8bbfc0093c21986d740247ae6dc6192b4
Stanislav Cortac <stanislav.cortac@eztemcorp.com>
2024-02-16 11:20:37 +0200

    fixed some js & styles on (checkout, header)

--- public/script/store/checkout.js
+++ public/script/store/checkout.js
@@ -1,41 +1,43 @@
 //<--------------------------------------------------------------------->
 //<--------------------------- Sticky Element -------------------------->
+stickyElement(".details");
+
 function stickyElement(elementSelector) {
   let element = document.querySelector(elementSelector);
 
   if (!element) {
     return;
-  } else {
-    let isActive = false;
-    let activationPosition = 10; // Poziția la care se activează funcționalitatea sticky
-
-    window.addEventListener("scroll", function () {
-      let scrollPosition = window.scrollY;
-
-      if (!isActive && scrollPosition > activationPosition) {
-        isActive = true;
-        let elementPosition = element.offsetTop + element.offsetHeight + 20;
-
-        window.addEventListener("scroll", function () {
-          let position = window.scrollY + window.innerHeight;
-
-          if (position > elementPosition) {
-            element.classList.remove("sticky");
-          } else {
-            element.classList.add("sticky");
-          }
-        });
-      } else if (isActive && scrollPosition == activationPosition) {
-        isActive = false;
-        element.classList.add("sticky"); // Adaugăm "sticky" înapoi când se revine la partea de sus
-      }
-    });
   }
+
+  let isActive = false;
+  let activationPosition = 10; // Poziția la care se activează funcționalitatea sticky
+  let elementPosition = element.offsetTop + element.offsetHeight + 20;
+  element.classList.add("sticky");
+
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+
+    if (!isActive && scrollPosition > activationPosition) {
+      isActive = true;
+      element.classList.add("sticky");
+    } else if (isActive && scrollPosition <= activationPosition) {
+      isActive = false;
+      element.classList.remove("sticky");
+    }
+
+    let position = window.scrollY + window.innerHeight;
+
+    if (position > elementPosition) {
+      element.classList.remove("sticky");
+    } else {
+      element.classList.add("sticky");
+    }
+  });
 }
+
 //<------------------------- End Sticky Element ------------------------>
 //<--------------------------------------------------------------------->
 //<------------------------ Start Functions IOS ------------------------>
-stickyElement(".details");
 //<---------------------- End Start Functions IOS ---------------------->
 //<--------------------------------------------------------------------->
 //<------------------------- Start Functions PC ------------------------>
