587e7f0b77da5cfec17ca9409370d2c80b5a2b76
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-02-23 13:48:10 +0200

    .

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Contracts\Encryption\DecryptException::class,
 
         /*
          * Package Service Providers...
