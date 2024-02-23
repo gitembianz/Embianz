6d61e5d9811d3afc53ce037e5e31c231943472a6
Mihail <git@embianz.com>
2024-02-23 16:14:16 +0200

    BackUp 90% Done Commit

--- resources/views/components/lazy.blade.php
+++ resources/views/components/lazy.blade.php
@@ -1,19 +1,19 @@
 <script>
-    const lastRecord = document.getElementById('last_record');
-    const options = {
-        root: null,
-        threshold: 1,
-        rootMargin: '0px'
-    }
+	const lastRecord = document.getElementById('last_record');
+	const options = {
+		root: null,
+		threshold: 1,
+		rootMargin: '0px'
+	}
 
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                @this.loadMore()
-                observer.unobserve(lastRecord);
-            }
-        });
-    }, options);
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				@this.loadMore()
+				observer.unobserve(lastRecord);
+			}
+		});
+	}, options);
 
-    observer.observe(lastRecord);
+	observer.observe(lastRecord);
 </script>
