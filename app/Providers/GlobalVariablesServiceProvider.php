15cb03a5c534a447001b77628ed7b2c41a2f2f6c
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-02-28 12:28:42 +0200

    back global scripts

--- app/Providers/GlobalVariablesServiceProvider.php
+++ app/Providers/GlobalVariablesServiceProvider.php
@@ -29,7 +29,7 @@ class GlobalVariablesServiceProvider extends ServiceProvider
         $this->loadGlobalVariables();
         $this->loadGlobalStatuses();
         $this->loadGlobalPayments();
-        // $this->loadGlobalCustomScripts();
+        $this->loadGlobalCustomScripts();
     }
     private function loadGlobalVariables()
     {
