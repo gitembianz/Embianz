449a20a812c0027cd6a2e948e1d76b4eaf43c998
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-02-28 13:08:39 +0200

    Custom Scripts - done!

--- database/migrations/2023_09_20_130136_create_vouchers_table.php
+++ database/migrations/2023_09_20_130136_create_vouchers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
       $table->id();
       $table->string('name');
       $table->string('code')->unique();
-      $table->decimal('percent', 5, 2);
+      $table->decimal('percent', 5, 2)->nullable();
       $table->unsignedBigInteger('status_id')->index()->nullable();
       $table->foreign('status_id')->references('id')->on('statuses');
       $table->boolean('single_use')->default(true);
