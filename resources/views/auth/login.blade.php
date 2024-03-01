00ddb3da329ad18c8e10b2cf3d07930007aad185
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-03-01 14:58:27 +0200

    Mdodify loghin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,44 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            
-        </x-slot>
+ <x-jet-authentication-card>
+  <x-slot name="logo">
 
-        <x-jet-validation-errors class="mb-4" />
+  </x-slot>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+  <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+  @if (session('status'))
+   <div class="mb-4 font-medium text-sm text-green-600">
+    {{ session('status') }}
+   </div>
+  @endif
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+  <form method="POST" action="{{ route('login') }}">
+   @csrf
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+   <div>
+    <x-jet-label for="email" value="{{ __('Email') }}" />
+    <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+     autofocus />
+   </div>
 
-            <div class="flex items-center justify-start mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+   <div class="mt-4">
+    <x-jet-label for="password" value="{{ __('Password') }}" />
+    <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+     autocomplete="current-password" />
+   </div>
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-            <div class="flex items-center justify-end mt-2">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                        {{ __('Don\'t have an account?') }}
-                    </a>
-                @endif
+   <div class="flex items-center justify-start mt-4">
+    @if (Route::has('password.request'))
+     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+      {{ __('Forgot your password?') }}
+     </a>
+    @endif
 
-            </div>
-        </form>
-    </x-jet-authentication-card>
+    <x-jet-button class="ml-4">
+     {{ __('Log in') }}
+    </x-jet-button>
+   </div>
+
+  </form>
+ </x-jet-authentication-card>
 </x-guest-layout>
