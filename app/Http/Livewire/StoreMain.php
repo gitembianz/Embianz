87fb54c47d921ed064b73853f31383a0090d69ae
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-03-21 16:34:08 +0200

    slider image too

--- app/Http/Livewire/StoreMain.php
+++ app/Http/Livewire/StoreMain.php
@@ -13,9 +13,10 @@ class StoreMain extends Component
 
   public function getSliderItemsProperty()
   {
-    return Category::select('id', 'slider_sequence')->where('slider_sequence', '!=', '0')->with(['media' => function ($query) {
-      $query->select('path', 'name', 'sequence')->where('type', 'original');
-    }])->orderby('sequence')->get();
+    return Category::select('id', 'slider_sequence')->where('slider_sequence', '!=', '0')->where('start_date', '<=',  now()->format('Y-m-d'))
+      ->where('end_date', '>=',  now()->format('Y-m-d'))->with(['media' => function ($query) {
+        $query->select('path', 'name', 'sequence')->where('type', 'original');
+      }])->orderby('sequence')->get();
   }
 
   private function getSessionId()
