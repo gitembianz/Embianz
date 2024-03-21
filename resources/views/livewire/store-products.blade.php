3b03527f5706f1f202758ebbebd12107bcd8e9bf
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-03-21 13:39:01 +0200

    inactive products

--- resources/views/livewire/store-products.blade.php
+++ resources/views/livewire/store-products.blade.php
@@ -81,10 +81,10 @@
 	<!---------------------------------------------------------->
 	<!-------------------------Catalogue------------------------>
 	<section class="catalogue container">
-		@if ($products->where('active', true)->isEmpty())
+		@if ($products->isEmpty())
 			<p>Nu au fost produse gasite</p>
 		@else
-			@foreach ($products->where('active', true) as $index => $product)
+			@foreach ($products as $index => $product)
 				<div class="product">
 					<div @if ($loop->last) id="last_record" @endif class="card" role="listitem">
 						<a href="{{ route("product", ["product" => $product->seo_id !== null && $product->seo_id !== "" ? $product->seo_id : $product->id]) }}">
