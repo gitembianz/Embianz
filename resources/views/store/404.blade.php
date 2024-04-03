f052d41fe1cc10989298373d988025c9473629cd
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-04-03 15:20:54 +0300

    Fixing bugs v-0.6 (Corectare numa pagini)

--- resources/views/store/404.blade.php
+++ resources/views/store/404.blade.php
@@ -12,10 +12,9 @@
     <a href="{{ url("/") }}">Întoarcete la pagina principală</a>
   </div>
   <script>
-		// Așteaptă 5 secunde (5000 milisecunde) și apoi face redirect către o altă pagină jkjkkj
 		setTimeout(function() {
 			window.location.href =
-				"/"; // Schimbă "pagina_de_redirect.html" cu adresa URL către care vrei să faci redirect
+				"/"; 
 		}, 5000);
 	</script>
 </main>
