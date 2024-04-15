ec4047713dcd7582f0eb1370d1fa4ba1ec3c3cb3
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-04-15 11:17:37 +0300

    Bug-Fix  0.5 - breadcrumbs corection

--- app/Http/Livewire/StoreProducts.php
+++ app/Http/Livewire/StoreProducts.php
@@ -56,10 +56,10 @@ class StoreProducts extends Component
     $this->specification = Specs::get();
     if ($category) {
       $decodedCategory = json_decode(htmlspecialchars_decode($category), true);
-      $this->category = Category::select('id', 'name', 'long_description')->find($decodedCategory['id']);
+      $this->category = Category::select('id', 'name', 'long_description', 'seo_id')->find($decodedCategory['id']);
     } else {
       if (app()->has('global_default_category')) {
-        $this->category = Category::select('id', 'name', 'long_description')->find(app('global_default_category')) ?? null;
+        $this->category = Category::select('id', 'name', 'long_description', 'seo_id')->find(app('global_default_category')) ?? null;
       }
     }
     $filteredValues = session()->get('filtered_values', []);
