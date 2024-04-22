9bfbe7dc8917b0a4abce66dd1804296d88ce196e
staniisllav <137802478+staniisllav@users.noreply.github.com>
2024-04-22 12:45:13 +0300

    Favicon FIX: image for all type of device & Google Search

--- resources/views/components/store-head.blade.php
+++ resources/views/components/store-head.blade.php
@@ -13,7 +13,20 @@
  {{-- DormiSoft Favicon --}}
  {{-- <link rel="icon" type="image/x-icon" href="/images/store/svg/dormisoft_favicon.svg"> --}}
  {{-- noren Favicon --}}
- <link rel="icon" type="image/x-icon" href="/images/store/svg/noren_favicon.svg">
+ {{-- <link rel="icon" type="image/x-icon" href="/images/store/svg/noren_favicon.svg"> --}}
+{{-- favicon --}}
+
+<link rel="apple-touch-icon" sizes="180x180" href="/images/store/svg/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/store/svg/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/store/svg/favicon-16x16.png">
+<link rel="manifest" href="/images/store/svg/site.webmanifest">
+<link rel="mask-icon" href="/images/store/svg/safari-pinned-tab.svg" color="#333333">
+<link rel="shortcut icon" href="/images/store/svg/favicon.ico">
+<meta name="msapplication-TileColor" content="#fafafa">
+<meta name="msapplication-config" content="/images/store/svg/browserconfig.xml">
+<meta name="theme-color" content="#fafafa">
+
+{{-- favicon end --}}
 
  <link rel="canonical" href="{{ url('/' . $canonical) }}">
  <link rel="preload" href="/dist/css/store.css" as="style">
