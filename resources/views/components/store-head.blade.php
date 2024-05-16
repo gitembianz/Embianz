20847aa7bb6b614e4920ee8d8bbb9ddeebaac975
staniisllav <137802478+staniisllav@users.noreply.github.com>
2024-05-16 18:34:12 +0300

    favicon fix

--- resources/views/components/store-head.blade.php
+++ resources/views/components/store-head.blade.php
@@ -19,6 +19,7 @@
 {{-- favicon --}}
 
 <link rel="apple-touch-icon" sizes="180x180" href="/images/store/svg/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="48x48" href="/images/store/svg/favicon-48x48.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/images/store/svg/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/images/store/svg/favicon-16x16.png">
 <link rel="manifest" href="/images/store/svg/site.webmanifest">
