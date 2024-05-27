23cd2199575db79004f86180dc15fdca72f7729c
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-05-27 17:40:02 +0300

    test.17- Iosif

--- resources/views/livewire/store-header.blade.php
+++ resources/views/livewire/store-header.blade.php
@@ -24,7 +24,7 @@
    <!-------------------------Logo------------------------->
 
    <a class="logo" href="{{ url('/') }}">
-    <img src="/images/store/svg/noren-black.svg" alt="MainLogo">
+    <img src="/images/store/svg/noren-black.svg" alt="Logo">
    </a>
    <!-----------------------END-Logo----------------------->
    <!------------------------------------------------------>
@@ -102,7 +102,7 @@
      </svg>
     </button>
     <a class="logo__hidden" href="{{ url('/') }}">
-     <img loading="lazy" src="/images/store/svg/noren-black.svg" alt="Site Logo">
+     <img src="/images/store/svg/noren-black.svg" alt="Site Logo">
     </a>
     {{-- wislist button --}}
     <button class="header__btn" wire:click="$emit('showwis')" id="wishOpen" aria-label="Open wishlist button">
