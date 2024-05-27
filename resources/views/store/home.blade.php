a9b827ce7ee6f5bd4bcc5e65b7cdf2f86d8532a8
Mihail <git@embianz.com>
2024-05-28 01:36:30 +0300

    300

--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -21,5 +21,5 @@
 @endphp
 
 <x-store-header />
-<img src="/images/store/default/default.webp" alt="something wrong" style="width:50%">
+<img src="/images/store/default/default300.webp" alt="something wrong" style="width:50%">
 <x-store-footer />
