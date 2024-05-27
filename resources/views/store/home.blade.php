3d6272e5a4c93232146de5d092f0b14e05f59ac2
Mihail <git@embianz.com>
2024-05-28 01:46:52 +0300

    default300

--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -1,25 +1,26 @@
-<x-store-head :title="getTitle()" :description="getDescription()" />
+<x-store-head 
+    :title="getTitle()" 
+    :description="getDescription()" 
+/>
 
 @php
- function getTitle()
- {
-     try {
-         return app('global_mainpage_metatitle');
-     } catch (\Throwable $e) {
-         return '';
-     }
- }
+function getTitle() {
+    try {
+        return app('global_mainpage_metatitle');
+    } catch (\Throwable $e) {
+        return '';
+    }
+}
 
- function getDescription()
- {
-     try {
-         return app('global_mainpage_metadescription');
-     } catch (\Throwable $e) {
-         return '';
-     }
- }
+function getDescription() {
+    try {
+        return app('global_mainpage_metadescription');
+    } catch (\Throwable $e) {
+        return '';
+    }
+}
 @endphp
 
 <x-store-header />
-<img src="/images/store/default/default300.webp" alt="something wrong" style="width:50%">
+@livewire('store-main')
 <x-store-footer />
