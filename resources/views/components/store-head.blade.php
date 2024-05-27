8213b68b0b08bca72d396a2e7f55478450f601c3
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-05-27 17:32:22 +0300

    test.15- Iosif

--- resources/views/components/store-head.blade.php
+++ resources/views/components/store-head.blade.php
@@ -55,7 +55,7 @@
  {{-- ----------------------------------------------------------- --}}
  <link rel="preload" href="/images/store/svg/noren-black.svg" as="image">
  <link rel="preload" href="/images/store/default/default300.webp" as="image">
- <link rel="preload" href="/images/store/svg/headset.svg" as="image">
+ {{-- <link rel="preload" href="/images/store/svg/headset.svg" as="image">
  <link rel="preload" href="/images/store/svg/truck.svg" as="image">
  <link rel="preload" href="/images/store/brands/dhl.webp" as="image">
  <link rel="preload" href="/images/store/brands/Fan.webp" as="image">
@@ -63,17 +63,14 @@
  <link rel="preload" href="/images/store/brands/visa.webp" as="image">
  <link rel="preload" href="/images/store/brands/mastercard.webp" as="image">
  <link rel="preload" href="/images/store/svg/chat.svg" as="image">
- <link rel="preload" href="/images/store/svg/thankyou.svg" as="image">
+ <link rel="preload" href="/images/store/svg/thankyou.svg" as="image"> --}}
  <link rel="preload" href="/images/store/svg/noren-white.svg" as="image">
  <link rel="preload" href="/fonts/Montserrat/Montserrat-Thin.woff2" as="font" type="font/woff2" crossorigin>
- <link rel="preload" href="/fonts/Montserrat/Montserrat-ExtraLight.woff2" as="font" type="font/woff2"
-  crossorigin>
+ <link rel="preload" href="/fonts/Montserrat/Montserrat-ExtraLight.woff2" as="font" type="font/woff2" crossorigin>
  <link rel="preload" href="/fonts/Montserrat/Montserrat-Light.woff2" as="font" type="font/woff2" crossorigin>
- <link rel="preload" href="/fonts/Montserrat/Montserrat-Regular.woff2" as="font" type="font/woff2"
-  crossorigin>
+ <link rel="preload" href="/fonts/Montserrat/Montserrat-Regular.woff2" as="font" type="font/woff2" crossorigin>
  <link rel="preload" href="/fonts/Montserrat/Montserrat-Medium.woff2" as="font" type="font/woff2" crossorigin>
- <link rel="preload" href="/fonts/Montserrat/Montserrat-SemiBold.woff2" as="font" type="font/woff2"
-  crossorigin>
+ <link rel="preload" href="/fonts/Montserrat/Montserrat-SemiBold.woff2" as="font" type="font/woff2" crossorigin>
  <link rel="preload" href="/fonts/Montserrat/Montserrat-Bold.woff2" as="font" type="font/woff2" crossorigin>
  <link rel="preload" href="/fonts/Montserrat/Montserrat-ExtraBold.woff2" as="font" type="font/woff2"
   crossorigin>
