069619a96c14cddd1d34930e53a02b855f4e4262
Mihail <git@embianz.com>
2024-05-29 13:20:16 +0300

    revert cache control

--- routes/web.php
+++ routes/web.php
@@ -133,12 +133,7 @@
 
   //simple page routes
 
- // route::view('/', 'store.home')->name('home')->middleware('cache-control');
-
-  Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
-    route::view('/', 'store.home')->name('home');
-    });
-
+  route::view('/', 'store.home')->name('home')->middleware('cache-control');
   route::view('/cart', 'store.cart')->name('cart');
   route::view('/wishlist', 'store.wislist')->name('wislist');
   route::view('/order', 'store.order')->name('order');
