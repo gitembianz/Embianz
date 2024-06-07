e631f9656c96c84b5e8f01526e4fef4daaf3b269
Mihail <git@embianz.com>
2024-06-07 15:17:44 +0300

    cache-control test

--- config/cache.php
+++ config/cache.php
@@ -52,6 +52,9 @@ return [
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
+            'headers' => [
+                'Cache-Control' => 'public',
+            ],
         ],
 
         'memcached' => [
