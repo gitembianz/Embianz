cbf7cca979598ff67ef944a7e2a37d96cb134424
staniisllav <stanislav.cortac@eztemcorp.com>
2024-06-17 17:08:43 +0300

    [EMB] Tooltips is added to all pages

--- resources/views/admin/add_pricelist.blade.php
+++ resources/views/admin/add_pricelist.blade.php
@@ -9,13 +9,13 @@
   <nav class="nav--controls">
     <h1 class="table--name">Add new price list</h1>
     {{-- Refresh Button --}}
-    <a class="button button--secondary button--centered" href="{{ route('pricelists') }}">
+    <a class="button button--secondary button--centered" tooltip="Back to Price Lists" tooltip-top href="{{ route('pricelists') }}">
       <svg><polyline points="15 18 9 12 15 6"></polyline></svg>
     </a>
-    <button class="button button--secondary button--centered" type="submit">
+    <button class="button button--secondary button--centered" tooltip="Save Price" tooltip-left type="submit">
       <svg><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M9 15l2 2l4 -4" /></svg>
     </button>
-    <button class="button button--secondary button--centered" type="reset">
+    <button class="button button--secondary button--centered" tooltip="Reset Price" tooltip-left type="reset">
       <svg><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
     </button>
   </nav>
