a94ea49a4fc35830bb48cf6c68a8cde67beddacc
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-06-28 13:56:56 +0300

    Fix 1.6 - no display parrents with no variants - fix

--- resources/views/livewire/product-details.blade.php
+++ resources/views/livewire/product-details.blade.php
@@ -103,73 +103,73 @@
    @if (count($variantGroup) > 1)
     <span class="product__price--title"
      style="margin-right: auto">{{ $product->beeingvariants->where('variant_id', $variantId)->first()->reference->name }}</span>
-   @endif
-   <div class="product__price">
-    <div class="variant__slider mini-slider">
-     <div class="variant__wrapper mini-wrapper">
-      <a class="variant__btn active"
-       href="{{ route('product', ['product' => $product->seo_id !== null && $product->seo_id !== '' ? $product->seo_id : $product->id]) }}">
-       @if ($product->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'image')
-        @if ($product->media->first() != null)
-         <img src="/{{ $product->media->first()->path }}{{ $product->media->first()->name }}"
-          alt="{{ $product->media->first()->name }}">
-        @else
-         <img src="/images/store/default/default70.webp" alt="something wrong">
-        @endif
-       @elseif ($product->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'text')
-        {{ $product->beeingvariants->where('variant_id', $variantId)->first()->value }}
-       @else
-        @if ($product->media->first() != null)
-         <img src="/{{ $product->media->first()->path }}{{ $product->media->first()->name }}"
-          alt="{{ $product->media->first()->name }}">
-        @else
-         <img src="/images/store/default/default70.webp" alt="something wrong">
-        @endif
-        {{ $product->beeingvariants->where('variant_id', $variantId)->first()->value }}
-       @endif
-      </a>
-      @foreach ($variantGroup as $variant)
-       @php
-        if ($variant->id == $product->id) {
-            continue;
-        }
-       @endphp
-       <a class="variant__btn"
-        href="{{ route('product', ['product' => $variant->seo_id !== null && $variant->seo_id !== '' ? $variant->seo_id : $variant->id]) }}">
-        @if ($variant->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'image')
-         @if ($variant->media->first() != null)
-          <img src="/{{ $variant->media->first()->path }}{{ $variant->media->first()->name }}"
-           alt="{{ $variant->media->first()->name }}">
+    <div class="product__price">
+     <div class="variant__slider mini-slider">
+      <div class="variant__wrapper mini-wrapper">
+       <a class="variant__btn active"
+        href="{{ route('product', ['product' => $product->seo_id !== null && $product->seo_id !== '' ? $product->seo_id : $product->id]) }}">
+        @if ($product->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'image')
+         @if ($product->media->first() != null)
+          <img src="/{{ $product->media->first()->path }}{{ $product->media->first()->name }}"
+           alt="{{ $product->media->first()->name }}">
          @else
           <img src="/images/store/default/default70.webp" alt="something wrong">
          @endif
-        @elseif ($variant->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'text')
-         {{ $variant->beeingvariants->where('variant_id', $variantId)->first()->value }}
+        @elseif ($product->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'text')
+         {{ $product->beeingvariants->where('variant_id', $variantId)->first()->value }}
         @else
-         @if ($variant->media->first() != null)
-          <img src="/{{ $variant->media->first()->path }}{{ $variant->media->first()->name }}"
-           alt="{{ $variant->media->first()->name }}">
-          {{ $variant->beeingvariants->where('variant_id', $variantId)->first()->value }}
+         @if ($product->media->first() != null)
+          <img src="/{{ $product->media->first()->path }}{{ $product->media->first()->name }}"
+           alt="{{ $product->media->first()->name }}">
          @else
           <img src="/images/store/default/default70.webp" alt="something wrong">
-          {{ $variant->beeingvariants->where('variant_id', $variantId)->first()->value }}
          @endif
+         {{ $product->beeingvariants->where('variant_id', $variantId)->first()->value }}
         @endif
        </a>
-      @endforeach
+       @foreach ($variantGroup as $variant)
+        @php
+         if ($variant->id == $product->id) {
+             continue;
+         }
+        @endphp
+        <a class="variant__btn"
+         href="{{ route('product', ['product' => $variant->seo_id !== null && $variant->seo_id !== '' ? $variant->seo_id : $variant->id]) }}">
+         @if ($variant->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'image')
+          @if ($variant->media->first() != null)
+           <img src="/{{ $variant->media->first()->path }}{{ $variant->media->first()->name }}"
+            alt="{{ $variant->media->first()->name }}">
+          @else
+           <img src="/images/store/default/default70.webp" alt="something wrong">
+          @endif
+         @elseif ($variant->beeingvariants->where('variant_id', $variantId)->first()->displayed == 'text')
+          {{ $variant->beeingvariants->where('variant_id', $variantId)->first()->value }}
+         @else
+          @if ($variant->media->first() != null)
+           <img src="/{{ $variant->media->first()->path }}{{ $variant->media->first()->name }}"
+            alt="{{ $variant->media->first()->name }}">
+           {{ $variant->beeingvariants->where('variant_id', $variantId)->first()->value }}
+          @else
+           <img src="/images/store/default/default70.webp" alt="something wrong">
+           {{ $variant->beeingvariants->where('variant_id', $variantId)->first()->value }}
+          @endif
+         @endif
+        </a>
+       @endforeach
+      </div>
+      <button class="variant__navigation variant__left mini-left">
+       <svg>
+        <polyline points="15 18 9 12 15 6"></polyline>
+       </svg>
+      </button>
+      <button class="variant__navigation variant__right mini-right">
+       <svg>
+        <polyline points="9 18 15 12 9 6"></polyline>
+       </svg>
+      </button>
      </div>
-     <button class="variant__navigation variant__left mini-left">
-      <svg>
-       <polyline points="15 18 9 12 15 6"></polyline>
-      </svg>
-     </button>
-     <button class="variant__navigation variant__right mini-right">
-      <svg>
-       <polyline points="9 18 15 12 9 6"></polyline>
-      </svg>
-     </button>
     </div>
-   </div>
+   @endif
   @endforeach
  @endif
 
