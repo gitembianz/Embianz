dad6d98fda1e74704372aa497587272bc19f8b88
staniisllav <stanislav.cortac@eztemcorp.com>
2024-07-05 09:59:13 +0300

    [EMB] All dropdowns & asides => Added wire:ignore

--- resources/views/livewire/paymentstable.blade.php
+++ resources/views/livewire/paymentstable.blade.php
@@ -5,8 +5,8 @@
 
  {{-- Asides --}}
  <aside>
-  <div class="background background--right" id="sort__backdrop"></div>
-  <div class="aside aside--right" id="sort">
+  <div class="background background--right" wire:ignore id="sort__backdrop"></div>
+  <div class="aside aside--right" wire:ignore id="sort">
    <div class="aside--controls">
     <button class="button button--flexed button--primary" id="sort__close">
      <svg>
@@ -32,8 +32,8 @@
   </div>
  </aside>
  <aside>
-  <div class="background background--right" id="visi__backdrop"></div>
-  <div class="aside aside--right" id="visi">
+  <div class="background background--right" wire:ignore id="visi__backdrop"></div>
+  <div class="aside aside--right" wire:ignore id="visi">
    <div class="aside--controls">
     <button class="button button--flexed button--primary" id="visi__close">
      <svg>
@@ -76,7 +76,7 @@
    </svg>
   </button>
   {{-- Sorting Dropdown --}}
-  <div class="dropdown dropdown--right display--desktop">
+  <div class="dropdown dropdown--right display--desktop" wire:ignore >
    {{-- Dropdown Button --}}
    <button class="button button--primary button--centered" tooltip="Sort items in table" tooltip-left>
     <svg>
@@ -104,7 +104,7 @@
    </div>
   </div>
   {{-- Visible Dropdown --}}
-  <div class="dropdown dropdown--right display--desktop">
+  <div class="dropdown dropdown--right display--desktop" wire:ignore >
    {{-- Dropdown Button --}}
    <button class="button button--primary button--centered" tooltip="Show items in table" tooltip-left>
     <svg>
