e27c859ad0261b36d46d7be18c9c402d9fd13d8a
Mihail <git@embianz.com>
2024-07-07 00:04:33 +0300

    fix anchor text menu mobile 2

--- resources/views/livewire/store-header.blade.php
+++ resources/views/livewire/store-header.blade.php
@@ -170,8 +170,8 @@
           <h4 style="margin-left: 7px">{{ $category->name }}</h4>
           {{-- <img loading="eager" class="heart__list--img" src="/images/store/default/default70.webp" alt="something wrong"> --}}
          @endif
+         <h4 style="margin-left: 7px">{{ $category->name }}</h4>
         </a>
-        <h4 style="margin-left: 7px">{{ $category->name }}</h4>
         <button class="dropmenu__open" href="#">
          <svg>
           <polyline points="6 9 12 15 18 9"></polyline>
