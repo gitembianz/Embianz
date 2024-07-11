e5c3cbeae2f8b1ef38bb65aa1599ea1a4e1174df
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-07-11 15:28:13 +0300

    Development 0.1 - 5stars on products

--- resources/views/livewire/product-details.blade.php
+++ resources/views/livewire/product-details.blade.php
@@ -14,8 +14,18 @@
    @if ($discount)
     <span class="product__discount">-{{ $product->product_prices->first()->discount }}%</span>
    @endif
-    <div class="rating" style="--rating: 69%;"></div>
+   @if (app()->has('global_display_rating') && app('global_display_rating') === 'true')
+    @php
+     $rating = 100 / (app('max_popularity') / $product->popularity);
+    @endphp
+    <div class="rating" style="--rating: {{ $rating }}%;">
+    </div>
+    @if (app()->has('global_display_rating_value') && app('global_display_rating_value') === 'true')
+     {{ $rating }}
+    @endif
+   @endif
   </div>
+
   @livewire('product-wishlist-button', [
       'productId' => $product->id,
       'class' => 'product__action',
