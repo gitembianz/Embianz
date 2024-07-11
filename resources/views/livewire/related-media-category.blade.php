e335083f7819d4c27528abb292ce600f03011776
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-07-11 12:01:18 +0300

    Fix 4.4 - category media fixed

--- resources/views/livewire/related-media-category.blade.php
+++ resources/views/livewire/related-media-category.blade.php
@@ -78,7 +78,14 @@
     <h1 class="table--name">
      {{ __('Add local media') }}
     </h1>
-    <input class="button button--primary button--centered" value="Save media" type="submit">
+    <button class="button button--primary button--centered" value="submit" type="submit">
+     <svg>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" />
+      <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+      <path d="M14 4l0 4l-6 0l0 -4" />
+     </svg>
+    </button>
     <button class="button button--danger button--centered" wire:click="cancel()">
      <svg>
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -89,7 +96,6 @@
      </svg>
     </button>
    </nav>
-
 
    {{-- Table --}}
    <div class="table" style="height: calc(100% - 60px);">
