301b4fe0ba1acd116eaf2c349099daac99a642d0
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-08-07 17:50:33 +0300

    Dev 0.3 - Wishlist->product from cache

--- resources/views/livewire/wishlist-products-list.blade.php
+++ resources/views/livewire/wishlist-products-list.blade.php
@@ -49,10 +49,10 @@
 
       <a class="leftbar__link"
        href="{{ route('product', ['product' => $item->product->seo_id !== null && $item->product->seo_id !== '' ? $item->product->seo_id : $item->product->id]) }}">
-       @if ($item->product->media->first())
+       @if ($item->product->media->where('type', 'min')->first())
         <img title="{{ $item->product->name }}" loading="eager" class="cart__list--img"
-         src="/{{ $item->product->media->first()->path }}{{ $item->product->media->first()->name }}"
-         alt="{{ $item->product->media->first()->name }} {{ $item->product->name }}">
+         src="/{{ $item->product->media->where('type', 'min')->first()->path }}{{ $item->product->media->where('type', 'min')->first()->name }}"
+         alt="{{ $item->product->media->where('type', 'min')->first()->name }} {{ $item->product->name }}">
        @else
         <img title="Default image" loading="eager" class="heart__list--img" src="/images/store/default/default70.webp"
          alt="something wrong">
