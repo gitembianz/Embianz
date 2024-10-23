2b85412607fc257057aa591aa0e6866b859ed31e
Mihail <git@embianz.com>
2024-10-23 23:35:53 +0300

    added svg favicon

--- resources/views/components/store-head.blade.php
+++ resources/views/components/store-head.blade.php
@@ -18,6 +18,7 @@
  <link rel="icon" type="image/png" sizes="48x48" href="/images/store/svg/favicon-48x48.png">
  <link rel="icon" type="image/png" sizes="32x32" href="/images/store/svg/favicon-32x32.png">
  <link rel="icon" type="image/png" sizes="16x16" href="/images/store/svg/favicon-16x16.png">
+ <link rel="icon" href="/images/store/svg/favicon.svg">
  <link rel="manifest" href="/images/store/svg/site.webmanifest">
  <link rel="mask-icon" href="/images/store/svg/safari-pinned-tab.svg" color="#333333">
  <link rel="shortcut icon" href="/images/store/svg/favicon.ico">
