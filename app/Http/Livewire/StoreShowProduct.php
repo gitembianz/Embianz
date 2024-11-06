b5e128ffe608bfd0cb072616e693f0dc13a527ec
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-11-06 17:12:05 +0200

    Dev 2.7 - Slider recent view products + produxt fix on add on cart

--- app/Http/Livewire/StoreShowProduct.php
+++ app/Http/Livewire/StoreShowProduct.php
@@ -14,11 +14,14 @@ class StoreShowProduct extends Component
   public $session_id;
   public $back = false;
   public $wishlistItems;
+  public $lastVisited;
+
 
   public function render()
   {
     return view('livewire.store-show-product', [
-      'product' => $this->product
+      'product' => $this->product,
+      'last_visited_products' => $this->lastproduct
     ]);
   }
   private function getSessionId()
@@ -34,7 +37,50 @@ class StoreShowProduct extends Component
     $this->productId = $productId;
     $this->session_id = $this->getSessionId();
     $this->wishlistItems = Wishlist::where('session_id', $this->session_id)->pluck('product_id')->toArray();
+
+    $this->lastVisited = json_decode(request()->cookie('last_visited_products', '[]'), true);
+    if (($key = array_search($productId, $this->lastVisited)) !== false) {
+      unset($this->lastVisited[$key]);
+    }
+    array_unshift($this->lastVisited, $productId);
+    cookie()->queue(cookie()->make('last_visited_products', json_encode($this->lastVisited), 60 * 24 * 30));
   }
+
+  public function getLastProductProperty()
+  {
+    if ($this->lastVisited) {
+      return Product::whereIn('id', $this->lastVisited)
+        ->orderByRaw("FIELD(id, " . implode(',', $this->lastVisited) . ")")
+        ->where('active', 1)
+        ->where('id', '!=', $this->productId)
+        ->where('start_date', '<=', now()->format('Y-m-d'))
+        ->where('end_date', '>=', now()->format('Y-m-d'))
+        ->select('id', 'name', 'sku', 'long_description', 'brand', 'popularity', 'seo_id', 'short_description', 'quantity', 'active', 'end_date', 'start_date')
+        ->with([
+          'media' => function ($query) {
+            $query->select('path', 'name', 'type')->where('type', 'main');
+          },
+          'reviews' => function ($query) {
+            $query->select('product_id', 'count', 'value');
+          },
+          'product_categories' => function ($query) {
+            $query->select('product_id', 'category_id', 'primary_category')
+              ->where('primary_category', true);
+            $query->with(['category' => function ($query) {
+              $query->select('id', 'short_description', 'seo_id');
+            }]);
+          },
+          'product_prices' => function ($query) {
+            $query->select('product_id', 'value', 'discount', 'value_no_discount');
+          },
+        ])
+        ->get() ?? collect();
+    } else {
+      return collect();
+    }
+  }
+
+
   public function isInWishlist($productId)
   {
     return in_array($productId, $this->wishlistItems);
