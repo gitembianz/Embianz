b749ec12a71b876783c42624faf7f32e6ea7e871
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-12-09 16:48:38 +0200

    Dev 3.3 - Promotions counter updated

--- resources/views/livewire/cart-products-list.blade.php
+++ resources/views/livewire/cart-products-list.blade.php
@@ -344,7 +344,7 @@
      @endif
     @endif
     @if ($timer > 0)
-     <h5 class="leftbar__total--text" style="color:red">
+     <h5 wire:ignore class="leftbar__total--text" style="color:red">
       @if (app()->has('label_cart_promotion_timer'))
        {!! app('label_cart_promotion_timer') !!}
       @endif
