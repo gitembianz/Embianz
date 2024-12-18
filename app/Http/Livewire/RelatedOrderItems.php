3cb6af86c3bb9c83214ceeab594b6978c5a74286
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-12-18 18:32:04 +0200

    Dev 3.9 - Order add order item

--- app/Http/Livewire/RelatedOrderItems.php
+++ app/Http/Livewire/RelatedOrderItems.php
@@ -3,8 +3,9 @@
 namespace App\Http\Livewire;
 
 use App\Models\Order;
-use App\Models\Order_Item;
+use App\Models\Product;
 use Livewire\Component;
+use App\Models\Order_Item;
 use Livewire\WithPagination;
 
 class RelatedOrderItems extends Component
@@ -26,18 +27,162 @@ class RelatedOrderItems extends Component
     public $columns = ['Id', 'Price', 'Quantity', 'VAT'];
     public $selectedColumns = [];
     public $order;
-    public $row = null;
+    public $rand = null;
+    public $rind2 = null;
+
     public $single = false;
     public $multiple = false;
+    public $additems = false;
+    //add declaration
+    public $productsAndValues = [];
+    public $searchadd = '';
+    public $showTable = false;
+    public $loadAmount = 20;
+    public $row = 1;
+
+    public function saveitems()
+    {
+        foreach ($this->productsAndValues as  $array) {
+            if (isset($array['product']['quantity']) && isset($array['product']['idrel'])) {
+                Order_Item::create([
+                    'order_id' => $this->orderId,
+                    'product_id' => $array['product']['idrel'],
+                    'price' => $array['price'],
+                    'quantity' => $array['product']['quantity'],
+                    'vat' => $array['vat']
+                ]);
+                $this->order->quantity_amount += $array['product']['quantity'];
+                $this->order->sum_amount += ($array['price'] * $array['product']['quantity']);
+                $this->order->save();
+                $this->order->final_amount = $this->order->sum_amount + $this->order->delivery_price - $this->order->promotion_value - $this->order->voucher_value;
+                $this->order->save();
+            } else {
+                session()->flash('notification', [
+                    'message' => 'Please provide values',
+                    'type' => 'warning',
+                    'title' => 'Missing Values'
+                ]);
+                return;
+            }
+        }
+
+        $this->productsAndValues = [];
+        $this->row = 1;
+        $this->additems = false;
+
+        session()->flash('notification', [
+            'message' => 'Record related successfully!',
+            'type' => 'success',
+            'title' => 'Success'
+        ]);
+    }
+
+    public function dennyselect($index)
+    {
+        $this->productsAndValues[$index]['allow'] = false;
+        $this->searchadd = '';
+    }
+    public function selectitem($index, $id, $name, $price, $vat)
+    {
+        $this->productsAndValues[$index]['itemselected'] = $name;
+        $this->productsAndValues[$index]['price'] = $price;
+        $this->productsAndValues[$index]['vat'] = $vat;
+
+        $this->productsAndValues[$index]['product']['idrel'] = $id;
+        $this->productsAndValues[$index]['allow'] = false;
+        $this->searchadd = '';
+    }
+    public function plus()
+    {
+        $this->row++;
+        $this->productsAndValues[] = [
+            'allow' => false,
+            'itemselected' => null,
+            'price' => null,
+            'vat' => null,
+            'product' => ['name' => null, 'quantity' => 1]
+        ];
+    }
+    public function clear($index)
+    {
+        unset($this->productsAndValues[$index]);
+
+        $this->productsAndValues = array_values($this->productsAndValues);
+
+        $this->row--;
+        if ($this->row < 1) {
+            $this->showTable = false;
+            $this->productsAndValues[] = [
+                'allow' => false,
+                'itemselected' => null,
+                'price' => null,
+                'vat' => null,
+                'product' => ['name' => null, 'quantity' => 1]
+            ];
+            $this->additems = false;
+            $this->row = 1;
+        }
+    }
+    public function allowselect($index)
+    {
+        foreach ($this->productsAndValues as &$item) {
+            $item['allow'] = false;
+        }
+        $this->productsAndValues[$index]['allow'] = true;
+        $this->searchadd = $this->productsAndValues[$index]['itemselected'];
+    }
+    public function closemodal()
+    {
+        $this->productsAndValues = [];
+        $this->productsAndValues[] = [
+            'allow' => false,
+            'itemselected' => null,
+            'price' => null,
+            'vat' => null,
+            'product' => ['name' => null, 'quantity' => 1]
+        ];
+        $this->row = 1;
+        $this->showTable = false;
+    }
+    public function getProductsProperty()
+    {
+        $relatedIds = $this->order->orders->pluck('product_id')->toArray();
+
+        // Start the query for products not in the relatedIds and not of type 'parent'
+        $unrelatedQuery = Product::whereNotIn('id', $relatedIds)
+            ->where('type', '!=', 'parent')
+            ->whereHas('product_prices', function ($query) {
+                $query->whereNotNull('value') // Ensure the price value is not null
+                    ->where('value', '>', 0); // Ensure the price value is greater than 0
+            });
+
+        // Add the search filter if $this->searchadd is not empty
+        if (!empty($this->searchadd)) {
+            $unrelatedQuery->where('name', 'like', '%' . $this->searchadd . '%');
+        }
+
+        return $unrelatedQuery->get();
+    }
+
 
     public function expandRow($index)
     {
-        if ($this->row  === null) {
-            $this->row = $index;
-        } elseif ($this->row != $index) {
-            $this->row = $index;
+        if ($this->rand  === null) {
+            $this->rand = $index;
+        } elseif ($this->rand != $index) {
+            $this->rand = $index;
         } else {
-            $this->row = null;
+            $this->rand = null;
+        }
+    }
+    public function expandRow2($index)
+    {
+        if ($this->rind2  === null) {
+            $this->rind2 = $index;
+        } elseif ($this->rind2 != $index) {
+            $this->rind2 = $index;
+        } else {
+            $this->rind2 = null;
         }
     }
 
@@ -53,13 +198,26 @@ class RelatedOrderItems extends Component
 
         return view('livewire.related-order-items', [
             'orderproducts' => $orderproducts,
+            'products' => $this->products,
+
         ]);
+    }
+    public function addorderitems()
+    {
+        $this->additems = true;
     }
     public function mount(Order $order)
     {
         $this->orderId = $order->id;
         $this->order = $order;
         $this->selectedColumns = $this->columns;
+        $this->productsAndValues[] = [
+            'allow' => false,
+            'itemselected' => null,
+            'price' => null,
+            'vat' => null,
+            'product' => ['name' => null, 'quantity' => 1]
+        ];
     }
     //function for related products
     public function showColumn($column)
