16b8fadd97e19b39edb2f4cd26891572ac529d33
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2024-12-19 14:38:30 +0200

    Dev 4.0 - Pagination links on sitemap

--- resources/views/livewire/storesettingstable.blade.php
+++ resources/views/livewire/storesettingstable.blade.php
@@ -73,17 +73,6 @@
     <path d="M19.94 11l0 .01" />
    </svg>
   </button>
-  {{-- Add store setting
-  <a class="button button--primary button--centered display--desktop" tooltip="Add new setting" tooltip-top
-   href="{{ route('add_storesetting') }}">
-   <svg>
-    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-    <polyline points="14 2 14 8 20 8"></polyline>
-    <line x1="12" y1="18" x2="12" y2="12"></line>
-    <line x1="9" y1="15" x2="15" y2="15"></line>
-   </svg>
-  </a> --}}
-  {{-- Add store setting --}}
 
   <button class="button button--primary button--centered display--desktop" tooltip="Reset cached filters" tooltip-top
    wire:click.prevent="refreshfilters">
@@ -206,15 +195,6 @@
       </svg>
       <span>Refresh table</span>
      </button>
-     <a class="button button--primary button--fill button--flexed" href="{{ route('add_storesetting') }}">
-      <svg>
-       <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-       <polyline points="14 2 14 8 20 8"></polyline>
-       <line x1="12" y1="18" x2="12" y2="12"></line>
-       <line x1="9" y1="15" x2="15" y2="15"></line>
-      </svg>
-      <span>Add store setting</span>
-     </a>
      <button class="button button--primary button--fill button--flexed" wire:click="actualizeaza">
       <svg>
        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
