ff09f66e62e630948f196911a683a137fd439d6c
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2025-01-06 09:31:14 +0200

    Dev 5.5 - Orderstable - mobile modified

--- resources/views/livewire/orderstable.blade.php
+++ resources/views/livewire/orderstable.blade.php
@@ -241,7 +241,7 @@
      </th>
      @foreach ($selectedColumns as $index => $column)
       @if ($this->showColumn($column))
-       <th @if ($index > count($selectedColumns) - 17) class="hidden" @endif>
+       <th @if ($index > 2) class="hidden" @endif>
         <button wire:click="sortBy('{{ $column }}')"
          class="table--btn @if ($orderBy === $column && $orderAsc === '1') active @endif">
          {{ str_replace('_id', '', $column) }}
@@ -281,7 +281,7 @@
         </div>
        </td>
        @foreach ($selectedColumns as $index => $column)
-        <td @if ($index > count($selectedColumns) - 17) class="hidden" @endif data-title="{{ $column }}"
+        <td @if ($index > 2) class="hidden" @endif data-title="{{ $column }}"
          wire:click="expandRow({{ $nr }})">
          @if ($column === 'name')
           <a href="{{ route('show_order', ['id' => $order->id]) }}">{{ $order->name }}</a>
@@ -323,13 +323,8 @@
        <td colspan="17">
         <div class="details">
          @foreach ($selectedColumns as $index => $column)
-          @if ($index >= count($selectedColumns) - 16)
-           @if ($column === 'name')
-            <p>
-             <bold>{{ $column }}:</bold>
-             <a href="{{ route('show_order', ['id' => $order->id]) }}">{{ $order->name }}</a>
-            </p>
-           @elseif ($column === 'account_id')
+          @if ($index > 2)
+           @if ($column === 'account_id')
             @if ($order->account_id)
              <p>
               <bold>{{ str_replace('_id', '', $column) }}:</bold>
