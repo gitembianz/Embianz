6df9d042262f17f8ca7de25151f3b368a34e046b
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2025-01-13 15:25:35 +0200

    Dormisoft- innerid on products

---
+++ database/migrations/2025_01_13_162421_add_column_to_products.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasColumn('products', 'innerid')) {
+
+            Schema::table('products', function (Blueprint $table) {
+                $table->integer('innerid')->nullable()->after('id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
