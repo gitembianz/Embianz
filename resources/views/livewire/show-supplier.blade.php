845d41d2a0ba5d42eda60afe590694ab90abd4f5
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2025-02-21 13:44:19 +0200

    Dev 6.9 - Suplier final,sum vat amount calculation.

--- resources/views/livewire/show-supplier.blade.php
+++ resources/views/livewire/show-supplier.blade.php
@@ -149,15 +149,15 @@
 
   {{-- Price List Create by --}}
   <div class="input__tabs">
-   <span class="disabled">{{ $supplier->final_amount }} {{ $supplier->currency }}</span>
+   <span class="disabled">{{ $supplier->final_amount }}</span>
    <label>Final amount</label>
   </div>
   <div class="input__tabs">
-   <span class="disabled">{{ $supplier->sum_amount }} {{ $supplier->currency }}</span>
+   <span class="disabled">{{ $supplier->sum_amount }}</span>
    <label>Sum amount</label>
   </div>
   <div class="input__tabs">
-   <span class="disabled">{{ $supplier->vat_sum_amount }} {{ $supplier->currency }}</span>
+   <span class="disabled">{{ $supplier->vat_sum_amount }}</span>
    <label>Vat amount</label>
   </div>
   {{-- Price List Create by --}}
