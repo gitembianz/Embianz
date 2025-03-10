ded98fbd96e1eab5344315d940d7c4e9e098de92
Iosifrelia <93663176+Iosifrelia@users.noreply.github.com>
2025-03-10 11:36:40 +0200

    Bugfix 7.5  - Delete exchange posibility

--- resources/views/livewire/pagestable.blade.php
+++ resources/views/livewire/pagestable.blade.php
@@ -206,7 +206,7 @@
       </svg>
       <span>Refresh table</span>
      </button>
-     <a class="button button--primary button--fill button--flexed" href="{{ route('add_page ') }}">
+     <a class="button button--primary button--fill button--flexed" href="{{ route('add_page') }}">
       <svg>
        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
        <polyline points="14 2 14 8 20 8"></polyline>
