c88b739aa0a3d91aa9bbf8faa3c48920e0a89e68
iosifrelia <iosif.relia@eztemcorp.com>
2025-09-08 09:10:08 +0300

    Fix 4.0 -  Multiple root elements detected

--- resources/views/livewire/cart-quantity.blade.php
+++ resources/views/livewire/cart-quantity.blade.php
@@ -1,9 +1,11 @@
- <div @if ($cart && $cart->quantity_amount != 0) style="display: flex !important"
-	@else style="display: none !important" @endif
-     id="cartCount" class="header__count">
-     <span>
-         @if ($cart->quantity_amount != 0)
-             {{ $cart->quantity_amount }}
-         @endif
-     </span>
- </div>
+<div>
+    <div @if ($cart && $cart->quantity_amount != 0) style="display: flex !important"
+         @else style="display: none !important" @endif
+         id="cartCount" class="header__count">
+        <span>
+            @if ($cart->quantity_amount != 0)
+                {{ $cart->quantity_amount }}
+            @endif
+        </span>
+    </div>
+</div>
