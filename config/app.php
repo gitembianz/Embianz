be3c870fc1932474f5939c8dbc1acc85ef000c70
Mihail <git@embianz.com>
2025-09-22 16:34:09 +0300

    telescope

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
         App\Providers\GlobalVariablesServiceProvider::class,
