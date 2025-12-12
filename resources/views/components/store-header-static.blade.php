0cc3201d9b76aaa29f314538e1abab1ebc6a5d57
Mihail <git@embianz.com>
2025-12-12 12:43:16 +0200

    remove @livewire('general-search') to test

--- resources/views/components/store-header-static.blade.php
+++ resources/views/components/store-header-static.blade.php
@@ -126,28 +126,8 @@
     </header>
 
 {{-- Static replacement for Livewire general-search --}}
-<div class="head__button__right">
-                    {{-- search button --}}
-                    @if (app()->has('global_display_search_button') && app('global_display_search_button') === 'true')
-                        <button class="header__btn" id="searchOpen"
-                            aria-label="Open Searchbar button">
-                            <svg>
-                                <circle cx="11" cy="11" r="8"></circle>
-                                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                            </svg>
-                        </button>
-                    @endif
-                    @if (app()->has('global_support_on') && app('global_support_on') === 'true')
-                        <a href="tel: @if (app()->has('global_support_phone_number')) {!! app('global_support_phone_number') !!} @endif"
-                            class="header__btn" aria-label="Call">
-                            <svg>
-                                <path
-                                    d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
-                                </path>
-                            </svg>
-                        </a>
-                    @endif
-@livewire('general-search')
+
+
 
     {{-- Cart products list placeholder --}}
     <div id="cart-products-list-container"></div>
