66bef5a36e33ea941529cb932c9c8127fad4d342
visiotech <iosifreliaa@gmail.com>
2026-01-04 19:53:45 +0200

    dev 0.1 add loading spinner

--- resources/views/livewire/cart-products-list.blade.php
+++ resources/views/livewire/cart-products-list.blade.php
@@ -2,7 +2,7 @@
     id="basketList">
     <button class="leftbar__hidden--close" wire:click="$set('showcart', false)" id="basketHidden"></button>
     <div class="leftbar__content" id="basketContent">
-        <div wire:loading.delay wire:target="increment, decrement, remove" class="wire-loading-container">
+        <div wire:loading.delay.long wire:target="continue" class="wire-loading-container">
             <div class="spinner"></div>
         </div>
 
