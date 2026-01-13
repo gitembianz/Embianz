3463385c60f4987a215c853f03354bc1ad2288ed
Mihail <git@embianz.com>
2026-01-13 02:26:42 +0200

    manual revert Iosif Changes.

--- app/Http/Livewire/ProductWishlistButton.php
+++ app/Http/Livewire/ProductWishlistButton.php
@@ -7,33 +7,50 @@ use App\Models\Wishlist;
 
 class ProductWishlistButton extends Component
 {
+    public $productId;
+    public $is_in_wishlist;
+    public $session_id;
+    public $class;
+    public $listeners = [];
 
-  protected $listeners = [
-    'wishlist:add' => 'add',
-    'wishlist:remove' => 'remove',
-  ];
-  public function render()
-  {
-    return view('livewire.product-wishlist-button');
-  }
+    public function mount($productId, $is_in_wishlist, $class)
+    {
+        $this->productId  = $productId;
+        $this->class  = $class;
 
+        $this->is_in_wishlist = $is_in_wishlist;
+        $this->session_id = request()->cookie('sessionId') ?? session()->getId();
 
-  public function add($productId)
-  {
-    Wishlist::updateOrCreate([
-      'session_id' => request()->cookie('sessionId') ?? session()->getId(),
-      'product_id' => $productId,
-    ]);
+        $this->listeners = ["update-wish-" . $this->productId => "refreshComponent"];
+    }
 
-    $this->emit('wishlistUpdated');
-  }
+    public function refreshComponent()
+    {
+        $this->is_in_wishlist = !$this->is_in_wishlist;
+        $this->mount($this->productId, $this->is_in_wishlist, $this->class);
+    }
 
-  public function remove($productId)
-  {
-    Wishlist::where('session_id', request()->cookie('sessionId') ?? session()->getId())
-      ->where('product_id', $productId)
-      ->delete();
+    public function addToWishlist($id)
+    {
+        Wishlist::updateOrCreate(
+            ['session_id' => $this->session_id, 'product_id' => $id]
+        );
+        $this->emit('wishlistUpdated');
+        $this->refreshComponent();
+    }
 
-    $this->emit('wishlistUpdated');
-  }
+    public function removeFromWishlist($id)
+    {
+        Wishlist::where('session_id', $this->session_id)
+            ->where('product_id', $id)
+            ->delete();
+
+        $this->emit('wishlistUpdated');
+        $this->refreshComponent();
+    }
+
+    public function render()
+    {
+        return view('livewire.product-wishlist-button');
+    }
 }
